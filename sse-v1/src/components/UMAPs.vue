d0018bf9ec472d50ad993ba4add02bea66765d45
Rémi Emonet <remi.emonet@reverse--com.heeere>
2021-09-18 11:37:22 +0200

    Improve UMAP all vs range ui

--- sse-v1/src/components/UMAPs.vue
+++ sse-v1/src/components/UMAPs.vue
@@ -35,22 +35,23 @@
     <ScatterChart v-bind="scatterChartProps" />
     <n-input-group>
       <div style="width: 10%; margin: 0 2%">
+        <n-space>
+          <n-switch
+            v-model:value="showAll"
+            title="Show all (or select aggregation window size)"
+          >
+            <template #checked>all</template>
+          </n-switch>
+          <n-button-group size="small">
+            <n-button @click="setDuration(600)">10min</n-button>
+            <n-button @click="setDuration(3600)">1h</n-button>
+          </n-button-group>
+        </n-space>
         <n-input-number
           v-model:value="duration"
           title="Display ... seconds"
+          :disabled="showAll"
         ></n-input-number>
-        <n-button-group size="small">
-          <n-button @click="duration = 600">10min</n-button>
-          <n-button @click="duration = 3600">1 hour</n-button>
-          <n-button
-            @click="
-              start = minStart;
-              duration = (maxStart - minStart) * 10;
-            "
-            type="primary"
-            >All</n-button
-          >
-        </n-button-group>
       </div>
       <div style="width: 84%">
         <n-input-group>
@@ -58,6 +59,7 @@
             :style="{ width: 80 / sliders.length + '%' }"
             v-for="s in sliders"
             :key="s.key"
+            :disabled="showAll"
             v-model:value="start"
             title="Display starting at..."
             :format-tooltip="(v) => `${dateThere(new Date(1000 * v))}`"
@@ -90,6 +92,7 @@ import {
   NInputGroup,
   NButton,
   NButtonGroup,
+  NSwitch,
 } from "naive-ui";
 const NComponents = {
   NTable,
@@ -98,6 +101,7 @@ const NComponents = {
   NInputGroup,
   NButton,
   NButtonGroup,
+  NSwitch,
 };
 
 import { ScatterChart, useScatterChart } from "vue-chart-3";
@@ -152,15 +156,20 @@ export default {
 
     onMounted(() => {
       select(
-        umaps.value[0], //parseInt(umaps.value.length / 2)],
-        bands.value[0] //parseInt(bands.value.length / 2)]
+        umaps.value[parseInt(umaps.value.length / 2)],
+        bands.value[parseInt(bands.value.length / 2)]
       );
     });
 
     const addAllGrey = ref(true);
-    const showAll = ref(false);
+    const showAll = ref(true);
     const duration = ref(3600); // sec
     const start = ref(0);
+
+    const setDuration = (sec) => {
+      showAll.value = false;
+      duration.value = sec;
+    };
 
     const fetcherCache = {};
     const fetcher = useTask(function* (signal, umap, band) {
@@ -229,14 +238,22 @@ export default {
     const chartData = computed(() => {
       if (fetcher.lastSuccessful === undefined) return { datasets: [] };
       const v = fetcher.lastSuccessful.value;
-      const tStart = start.value;
-      const tEnd = tStart + duration.value;
       const labels = [...new Set(v.l)];
+      const rawData = () => v.X.map(([x, y]) => ({ x, y }));
+      let data;
+      if (!showAll.value) {
+        const tStart = start.value;
+        const tEnd = tStart + duration.value;
+        data = (l) =>
+          rawData().filter(
+            (o, i) => v.l[i] === l && v.t[i] >= tStart && v.t[i] < tEnd
+          );
+      } else {
+        data = (l) => rawData().filter((o, i) => v.l[i] === l);
+      }
       const datasets = labels.map((l, il) => ({
         label: l,
-        data: v.X.map(([x, y]) => ({ x, y })).filter(
-          (o, i) => v.l[i] === l && v.t[i] >= tStart && v.t[i] < tEnd
-        ),
+        data: data(l),
         backgroundColor: `hsl(${il / labels.length}turn, 100%, 50%, 1)`,
       }));
       const allGreyDataset =
@@ -299,6 +316,8 @@ export default {
       sliders,
       start,
       duration,
+      setDuration,
+      showAll,
       scatterChartProps,
       currentBand,
       currentUmap,
