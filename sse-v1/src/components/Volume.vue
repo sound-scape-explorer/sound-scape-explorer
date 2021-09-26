7017a75a6d3d2de066aa29d1d436f7f8e76225fb
Rémi Emonet <remi.emonet@reverse--com.heeere>
2021-09-27 00:10:22 +0200

    Volume boxplot with client-side re-aggregation

--- sse-v1/src/components/Volume.vue
+++ sse-v1/src/components/Volume.vue
@@ -3,30 +3,15 @@
     <input
       class="focus"
       ref="focus"
-      @keydown.left="
-        currentBand = bands[Math.max(0, bands.indexOf(currentBand) - 1)]
-      "
-      @keydown.right="
-        currentBand =
-          bands[Math.min(bands.length - 1, bands.indexOf(currentBand) + 1)]
-      "
-      @keydown.up="
-        currentIntegration =
-          integrations[
-            Math.max(0, integrations.indexOf(currentIntegration) - 1)
-          ]
-      "
-      @keydown.down="
-        currentIntegration =
-          integrations[
-            Math.min(
-              integrations.length - 1,
-              integrations.indexOf(currentIntegration) + 1
-            )
-          ]
-      "
+      @keydown.left="select(undefined, -1)"
+      @keydown.right="select(undefined, +1)"
+      @keydown.up="select(-1, undefined)"
+      @keydown.down="select(1, undefined)"
     />
+    <input v-model="currentRangeName" />
+    <input v-model="currentSite" />
     <input v-model="what" />
+    <input v-model.number="aggregate" />
     <BoxPlotChart
       :chartData="boxPlotData"
       :options="{
@@ -84,34 +69,140 @@ const NComponents = { NTable };
 import { defineChartComponent } from "vue-chart-3";
 const BoxPlotChart = defineChartComponent("BoxPlotChart", "boxplot");
 
+import { FILE_SITE } from "@/mappings.js";
+import { useTask } from "vue-concurrency";
+import { inject, computed, ref } from "vue";
+
 export default {
   inject: ["root"],
   components: { ...NComponents, BoxPlotChart },
-  data: () => ({
-    currentBand: "",
-    currentIntegration: "",
-    what: "sumvar",
-  }),
   mounted() {
     this.currentBand = this.bands[parseInt(this.bands.length / 2)];
     this.currentIntegration =
       this.integrations[parseInt(this.integrations.length / 2)];
+    this.currentRangeName = Object.keys(this.root.cfg.ranges)[0];
+    this.currentSite = [
+      ...new Set(Object.values(this.root.cfg.files).map((f) => f[FILE_SITE])),
+    ][0];
     this.$refs.focus.focus();
   },
+  setup() {
+    const root = inject("root");
+    // data-like
+    const o = {
+      currentIntegration: ref(""),
+      currentBand: ref(""),
+      currentRangeName: ref(""),
+      currentSite: ref(""),
+      what: ref("sumvar"),
+      aggregate: ref(30),
+      focus: ref(null),
+    };
+    o.bands = computed(() => Object.keys(root.cfg.bands));
+    o.integrations = computed(() =>
+      root.cfg.variables.integration_seconds.split("-")
+    );
+
+    const fetcherCache = {};
+    o._fetcher = useTask(function* (signal, inte, band) {
+      const k = inte + "/" + band;
+      if (k in fetcherCache) return fetcherCache[k];
+      const req = yield fetch(
+        `${root.BASE}generated/single/volume/${inte}/${band}.json`,
+        { signal }
+      );
+      const json = yield req.json();
+      fetcherCache[k] = json;
+      return json;
+    });
+
+    o.select = function (ki, k) {
+      console.log(ki, k, o.bands, o.currentBand);
+      if (k !== undefined) {
+        if (typeof k === "number") {
+          let ik = Math.min(
+            Math.max(0, o.bands.value.indexOf(o.currentBand.value) + k),
+            o.bands.value.length - 1
+          );
+          o.currentBand.value = o.bands.value[ik];
+        } else {
+          o.currentBand.value = k;
+        }
+      }
+      if (ki !== undefined) {
+        if (typeof ki === "number") {
+          let iki = Math.min(
+            Math.max(
+              0,
+              o.integrations.value.indexOf(o.currentIntegration.value) + ki
+            ),
+            o.integrations.value.length - 1
+          );
+          o.currentIntegration.value = o.integrations.value[iki];
+        } else {
+          o.currentIntegration.value = ki;
+        }
+      }
+      o._fetcher.perform(o.currentIntegration.value, o.currentBand.value);
+      o.focus.value.focus();
+    };
+
+    o.boxPlotData = computed(() => {
+      if (o._fetcher.lastSuccessful === undefined) return { datasets: [] };
+      const v = o._fetcher.lastSuccessful.value;
+      /*const sites = [
+        ...new Set(Object.values(root.cfg.files).map((f) => f[FILE_SITE])),
+      ];*/
+      const site = o.currentSite.value;
+      const res = {
+        labels: [],
+        datasets: [
+          {
+            label: site,
+            backgroundColor: `hsla(${(0 * 360) / 1},50%,50%,0.5)`,
+            borderColor: "black",
+            borderWidth: 1,
+            outlierBackgroundColor: "red",
+            data: [],
+          },
+        ],
+      };
+      const k = o.currentRangeName.value + " " + site;
+      const times = v.data[k].t;
+      const series = v.data[k][o.what.value];
+      let currentTime = -Infinity;
+      let currentData = [];
+      const commit = (nextT) => {
+        if (currentData.length > 0) {
+          res.labels.push(
+            new Date(currentTime * 1000).toISOString().replace(".000Z", "")
+          );
+          res.datasets[0].data.push(currentData);
+        }
+        currentTime = nextT;
+        currentData = [];
+      };
+      for (const i in times) {
+        const t = times[i];
+        if (t >= currentTime + o.aggregate.value) {
+          commit(t);
+        }
+        currentData.push(series[i]);
+      }
+      commit(+Infinity);
+      return res;
+    });
+
+    return o;
+  },
   computed: {
-    bands() {
-      return Object.keys(this.root.cfg.bands);
-    },
-    integrations() {
-      return this.root.cfg.variables.integration_seconds.split("-");
-    },
     currentGraphURL() {
       if (this.currentBand === "") return "";
       const B = this.root.BASE + this.root.cfg.variables.generated_base;
       const inte = parseInt(this.currentIntegration);
       return `${B}single/volume/${inte}/${this.currentBand}.${this.what}.png`;
     },
-    boxPlotData() {
+    boxPlotDataTest() {
       const res = {
         labels:
           "1 2 3 4 5 6 very-long-stuff-of-the-dead-too-long-for-sure".split(
@@ -142,17 +233,7 @@ export default {
       return res;
     },
   },
-  methods: {
-    select(ki, k) {
-      if (k !== undefined) {
-        this.currentBand = k;
-      }
-      if (ki !== undefined) {
-        this.currentIntegration = ki;
-      }
-      this.$refs.focus.focus();
-    },
-  },
+  methods: {},
 };
 </script>
 
--- sse-v1/src/mappings.js
+++ sse-v1/src/mappings.js
@@ -1,3 +1,5 @@
+/* indices in the list-serialized tuples from python */
 
 const UMAP_RANGES = 3;
-export { UMAP_RANGES }; 
+const FILE_SITE = 0;
+export { UMAP_RANGES, FILE_SITE }
