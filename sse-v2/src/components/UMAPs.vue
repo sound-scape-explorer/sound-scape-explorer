cc890637908129e70cd57c510625399762ddfaf1
Rémi Emonet <remi.emonet@reverse--com.heeere>
2022-03-15 17:30:49 +0100

    Pure automated reformatting

--- sse-v2/src/components/UMAPs.vue
+++ sse-v2/src/components/UMAPs.vue
@@ -48,7 +48,8 @@
         <n-button-group>
           <label
             >Showing {{ duration }} seconds, starting at
-            {{ dateThere(new Date(1000 * start )) }} {{dateThere(new Date())}}</label
+            {{ dateThere(new Date(1000 * start)) }}
+            {{ dateThere(new Date()) }}</label
           >
         </n-button-group>
       </div>
@@ -98,211 +99,213 @@ import { UMAP_RANGES } from "@/mappings.js";
 import { onMounted, inject, computed, ref } from "vue";
 import { useTask } from "vue-concurrency";
 
-    const root = inject("root");
-    const focus = ref(null);
-    const currentBand = ref("");
-    const currentUmap = ref("");
-    const clearScatter = ref(true);
+const root = inject("root");
+const focus = ref(null);
+const currentBand = ref("");
+const currentUmap = ref("");
+const clearScatter = ref(true);
 
-    const bands = computed(() => Object.keys(root.cfg.bands));
-    const umaps = computed(() => Object.keys(root.cfg.umaps));
+const bands = computed(() => Object.keys(root.cfg.bands));
+const umaps = computed(() => Object.keys(root.cfg.umaps));
 
-    const currentGraphURL = computed(() => {
-      if (currentBand.value === "") return "";
-      const B = root.BASE + root.cfg.variables.generated_base;
-      return B + `umap/${currentUmap.value}/${currentBand.value}.png`;
-    });
+const currentGraphURL = computed(() => {
+  if (currentBand.value === "") return "";
+  const B = root.BASE + root.cfg.variables.generated_base;
+  return B + `umap/${currentUmap.value}/${currentBand.value}.png`;
+});
 
-    function select(ku, k) {
-      clearScatter.value = true
-      if (k !== undefined) {
-        if (typeof k === "number") {
-          let ik = Math.min(
-            Math.max(0, bands.value.indexOf(currentBand.value) + k),
-            bands.value.length - 1
-          );
-          currentBand.value = bands.value[ik];
-        } else {
-          currentBand.value = k;
-        }
-      }
-      if (ku !== undefined) {
-        if (typeof ku === "number") {
-          let iku = Math.min(
-            Math.max(0, umaps.value.indexOf(currentUmap.value) + ku),
-            umaps.value.length - 1
-          );
-          currentUmap.value = umaps.value[iku];
-        } else {
-          currentUmap.value = ku;
-        }
-      }
-      fetcher.perform(currentUmap.value, currentBand.value);
-      focus.value?.focus();
+function select(ku, k) {
+  clearScatter.value = true;
+  if (k !== undefined) {
+    if (typeof k === "number") {
+      let ik = Math.min(
+        Math.max(0, bands.value.indexOf(currentBand.value) + k),
+        bands.value.length - 1
+      );
+      currentBand.value = bands.value[ik];
+    } else {
+      currentBand.value = k;
     }
-
-    onMounted(() => {
-      select(
-        umaps.value[Math.floor(umaps.value.length / 2)],
-        bands.value[Math.floor(bands.value.length / 2)]
+  }
+  if (ku !== undefined) {
+    if (typeof ku === "number") {
+      let iku = Math.min(
+        Math.max(0, umaps.value.indexOf(currentUmap.value) + ku),
+        umaps.value.length - 1
       );
-    });
+      currentUmap.value = umaps.value[iku];
+    } else {
+      currentUmap.value = ku;
+    }
+  }
+  fetcher.perform(currentUmap.value, currentBand.value);
+  focus.value?.focus();
+}
 
-    const addAllGrey = ref(true);
-    const showAll = ref(true);
-    const duration = ref(3600); // sec
-    const start = ref(Math.floor(Date.now()/1000));// do not patch here the date by timezone, directly on display 
+onMounted(() => {
+  select(
+    umaps.value[Math.floor(umaps.value.length / 2)],
+    bands.value[Math.floor(bands.value.length / 2)]
+  );
+});
 
-    const setDuration = (sec) => {
-      showAll.value = false;
-      duration.value = sec;
-    };
+const addAllGrey = ref(true);
+const showAll = ref(true);
+const duration = ref(3600); // sec
+const start = ref(Math.floor(Date.now() / 1000)); // do not patch here the date by timezone, directly on display
 
-    // TODO: perf: should probably avoid reactivity on the data? is it fetcher or vuechart?
-    const fetcherCache = {};
-    const fetcher = useTask(function* (signal, umap, band) {
-      const k = umap + "/" + band;
-      console.log(k, fetcherCache);
-      if (k in fetcherCache) {
-        setTimeout(() => { clearScatter.value = false; }, 200);
-        return fetcherCache[k];
-      }
-      const req = yield fetch(
-        `${root.BASE}generated/umap/${umap}/${band}.json`,
-        { signal }
-      );
-      const json = yield req.json();
-      fetcherCache[k] = json;
-      setTimeout(() => { clearScatter.value = false; }, 200);
-      return json;
-    });
+const setDuration = (sec) => {
+  showAll.value = false;
+  duration.value = sec;
+};
 
-    const startStep = computed(() => Math.floor(duration.value / 2));
-    const minStart = computed(() =>
-      fetcher.lastSuccessful ? Math.min(...fetcher.lastSuccessful.value.t) : 0
-    );
-    const maxStart = computed(() =>
-      fetcher.lastSuccessful
-        ? minStart.value +
+// TODO: perf: should probably avoid reactivity on the data? is it fetcher or vuechart?
+const fetcherCache = {};
+const fetcher = useTask(function* (signal, umap, band) {
+  const k = umap + "/" + band;
+  console.log(k, fetcherCache);
+  if (k in fetcherCache) {
+    setTimeout(() => {
+      clearScatter.value = false;
+    }, 200);
+    return fetcherCache[k];
+  }
+  const req = yield fetch(`${root.BASE}generated/umap/${umap}/${band}.json`, {
+    signal,
+  });
+  const json = yield req.json();
+  fetcherCache[k] = json;
+  setTimeout(() => {
+    clearScatter.value = false;
+  }, 200);
+  return json;
+});
+
+const startStep = computed(() => Math.floor(duration.value / 2));
+const minStart = computed(() =>
+  fetcher.lastSuccessful ? Math.min(...fetcher.lastSuccessful.value.t) : 0
+);
+const maxStart = computed(() =>
+  fetcher.lastSuccessful
+    ? minStart.value +
+      startStep.value *
+        Math.floor(
+          (Math.max(...fetcher.lastSuccessful.value.t) - minStart.value) /
+            startStep.value
+        )
+    : 0
+);
+const sliders = computed(() => {
+  if (root.cfg.umaps[currentUmap.value] === undefined) return [];
+  return root.cfg.umaps[currentUmap.value][UMAP_RANGES].map((kr) => ({
+    key: kr,
+    startStop: root.cfg.ranges[kr].map((d) => {
+      console.log(Math.floor(new Date(d).getTime() / 1000), d);
+      return parseInt(new Date(d).getTime() / 1000);
+    }),
+    marks: {
+      [Math.floor(new Date(root.cfg.ranges[kr][0]).getTime() / 1000)]: "⟦ ",
+      [Math.floor(
+        new Date(root.cfg.ranges[kr][0]).getTime() / 1000 +
           startStep.value *
             Math.floor(
-              (Math.max(...fetcher.lastSuccessful.value.t) - minStart.value) /
-                startStep.value
+              (new Date(root.cfg.ranges[kr][1]).getTime() -
+                new Date(root.cfg.ranges[kr][0]).getTime()) /
+                1000 /
+                startStep.value /
+                2
             )
-        : 0
-    );
-    const sliders = computed(() => {
-      if (root.cfg.umaps[currentUmap.value] === undefined) return [];
-      return root.cfg.umaps[currentUmap.value][UMAP_RANGES].map((kr) => ({
-        key: kr,
-        startStop: root.cfg.ranges[kr].map((d) =>{
-          console.log(Math.floor(new Date(d).getTime() / 1000 ), d)
-          return parseInt(new Date(d).getTime() / 1000 ) 
-        }),
-        marks: {
-          [Math.floor(new Date(root.cfg.ranges[kr][0]).getTime() / 1000)]: "⟦ ",
-          [Math.floor(
-            new Date(root.cfg.ranges[kr][0]).getTime() / 1000 +
-              startStep.value *
-                Math.floor(
-                  (new Date(root.cfg.ranges[kr][1]).getTime() -
-                    new Date(root.cfg.ranges[kr][0]).getTime()) /
-                    1000 /
-                    startStep.value /
-                    2
-                )
-          )]: kr,
-          [Math.floor(new Date(root.cfg.ranges[kr][1]).getTime() / 1000)]: "⟧",
-        },
-      }));
-    });
-    function dateThere(d) {
-      return new Intl.DateTimeFormat("fr", {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        timeZone: root.cfg.variables.display_locale,
-      }).format(d);
-    }
+      )]: kr,
+      [Math.floor(new Date(root.cfg.ranges[kr][1]).getTime() / 1000)]: "⟧",
+    },
+  }));
+});
+function dateThere(d) {
+  return new Intl.DateTimeFormat("fr", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZone: root.cfg.variables.display_locale,
+  }).format(d);
+}
 
-    const chartData = computed(() => {
-      if (fetcher.lastSuccessful === undefined) return null; // TODO null is ok but when nb of datasets changes there is a crash
-      const v = fetcher.lastSuccessful.value;
-      const labels = [...new Set(v.l)];
-      const rawData = () => v.X.map(([x, y]) => ({ x, y }));
-      let data;
-      if (!showAll.value) {
-        const tStart = start.value;
-        const tEnd = tStart + duration.value;
-        data = (l) =>
-          rawData().filter(
-            (o, i) => v.l[i] === l && v.t[i] >= tStart && v.t[i] < tEnd
-          );
-      } else {
-        data = (l) => rawData().filter((o, i) => v.l[i] === l);
-      }
-      const datasets = labels.map((l, il) => ({
-        label: l,
-        data: data(l),
-        backgroundColor: `hsl(${il / labels.length}turn, 75%, 50%, .5)`,
-        // TODO: have a common hsl h-bending function that better distributes in terms of Δe00 for instance
-      }));
-      const allGreyDataset =
-        true || addAllGrey.value && !showAll.value // TODO: when nb of datasets changes, there is a crash
-          ? [
-              {
-                label: "*",
-                data: v.X.map(([x, y]) => ({ x, y })),
-                pointBackgroundColor: "hsl(0, 0%, 90%)",
-                pointBorderWidth: 0,
-                pointRadius: 2,
-              },
-            ]
-          : [];
+const chartData = computed(() => {
+  if (fetcher.lastSuccessful === undefined) return null; // TODO null is ok but when nb of datasets changes there is a crash
+  const v = fetcher.lastSuccessful.value;
+  const labels = [...new Set(v.l)];
+  const rawData = () => v.X.map(([x, y]) => ({ x, y }));
+  let data;
+  if (!showAll.value) {
+    const tStart = start.value;
+    const tEnd = tStart + duration.value;
+    data = (l) =>
+      rawData().filter(
+        (o, i) => v.l[i] === l && v.t[i] >= tStart && v.t[i] < tEnd
+      );
+  } else {
+    data = (l) => rawData().filter((o, i) => v.l[i] === l);
+  }
+  const datasets = labels.map((l, il) => ({
+    label: l,
+    data: data(l),
+    backgroundColor: `hsl(${il / labels.length}turn, 75%, 50%, .5)`,
+    // TODO: have a common hsl h-bending function that better distributes in terms of Δe00 for instance
+  }));
+  const allGreyDataset =
+    true || (addAllGrey.value && !showAll.value) // TODO: when nb of datasets changes, there is a crash
+      ? [
+          {
+            label: "*",
+            data: v.X.map(([x, y]) => ({ x, y })),
+            pointBackgroundColor: "hsl(0, 0%, 90%)",
+            pointBorderWidth: 0,
+            pointRadius: 2,
+          },
+        ]
+      : [];
 
-      return {
-        datasets: [...datasets, ...allGreyDataset],
-      };
-    });
-    const chartOptions = computed(() => {
-      if (fetcher.lastSuccessful === undefined) return {};
-      const v = fetcher.lastSuccessful.value;
-      const xs = v.X.map((pair) => pair[0]);
-      const ys = v.X.map((pair) => pair[1]);
-      const scales = {
-        x: {
-          min: Math.min(...xs),
-          max: Math.max(...xs),
-        },
-        y: {
-          min: Math.min(...ys),
-          max: Math.max(...ys),
-        },
-      };
-      const addx = (scales.x.max - scales.x.min) / 20;
-      const addy = (scales.y.max - scales.y.min) / 20;
-      scales.x.min -= addx;
-      scales.x.max += addx;
-      scales.y.min -= addy;
-      scales.y.max += addy;
+  return {
+    datasets: [...datasets, ...allGreyDataset],
+  };
+});
+const chartOptions = computed(() => {
+  if (fetcher.lastSuccessful === undefined) return {};
+  const v = fetcher.lastSuccessful.value;
+  const xs = v.X.map((pair) => pair[0]);
+  const ys = v.X.map((pair) => pair[1]);
+  const scales = {
+    x: {
+      min: Math.min(...xs),
+      max: Math.max(...xs),
+    },
+    y: {
+      min: Math.min(...ys),
+      max: Math.max(...ys),
+    },
+  };
+  const addx = (scales.x.max - scales.x.min) / 20;
+  const addy = (scales.y.max - scales.y.min) / 20;
+  scales.x.min -= addx;
+  scales.x.max += addx;
+  scales.y.min -= addy;
+  scales.y.max += addy;
 
-      return {
-        title: `UMAP [${currentUmap.value}] ${currentBand.value}, ${v.binSize}sec window`,
-        animation: {
-          duration: 0,
-        },
-        scales,
-      };
-    });
+  return {
+    title: `UMAP [${currentUmap.value}] ${currentBand.value}, ${v.binSize}sec window`,
+    animation: {
+      duration: 0,
+    },
+    scales,
+  };
+});
 
-    const { scatterChartProps } = useScatterChart({
-      chartData,
-      options: chartOptions,
-    });
-
+const { scatterChartProps } = useScatterChart({
+  chartData,
+  options: chartOptions,
+});
 </script>
 
 <style>
