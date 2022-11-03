9fb4397371f0e288eba03a03dc0b0efe4dff3d74
Bamdad Sabbagh <hi@bamdad.fr>
2022-11-03 11:08:44 +0100

    fix(Processing): Add state feedback to console

--- processing/run.sh
+++ processing/run.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-cd /sample || exit 0
+function print_start {
+  echo ""
+  echo "-----------------------"
+  echo "Processing: Starting..."
+  echo "-----------------------"
+  echo ""
+}
 
-mkdir generated || exit 0
+function print_end {
+  echo ""
+  echo "---------------------"
+  echo "Processing: Ending..."
+  echo "---------------------"
+  echo ""
+}
 
-mkdir features || exit 0
+function print_abort {
+  echo ""
+  echo "----------------------"
+  echo "Processing: Aborted..."
+  echo "----------------------"
+  echo ""
 
-echo ""
-echo "--------------------------------"
-echo "SSE: Wait for processing to end!"
-echo "--------------------------------"
-echo ""
+  exit 0
+}
+
+cd /sample || print_abort
+
+mkdir generated || print_abort
+
+mkdir features || print_abort
+
+print_start
 
 sse extract all
 
 sse compute volume && sse compute covering && sse compute umap
 
 sse show config --json > generated/ghost-config.json
+
+print_end
