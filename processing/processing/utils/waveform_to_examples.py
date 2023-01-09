e8b8147944d755e7bd5cd93a25a98fcc33062aaf
JeremyRUJM <jeremy.rouch@univ-st-etienne.fr>
2023-01-09 14:09:57 +0100

    fix(Processing): Using audio chunks of 1 sec

--- processing/processing/utils/waveform_to_examples.py
+++ processing/processing/utils/waveform_to_examples.py
@@ -1,5 +1,7 @@
 import torch
 import torchaudio
+import numpy as np
+import scipy.signal as signal
 
 from processing.utils.frame import frame
 
@@ -11,10 +13,20 @@ def waveform_to_examples(data, sample_rate, band_params):
     if ntot % 64 > 0:
         raise Exception(f'The band size parameter should be a multiple of 64')
     n_fft = 4096 // 2 * (ntot // 64)  # (4096*3 * 64) //ntot
+    
+    # if sample_rate % 100 != 0:
+    #     sample_rate2 = np.lcm(sample_rate,100)
+    #     data = torchaudio.transforms.Resample(
+    #         orig_freq=sample_rate, new_freq=sample_rate2).to(data.device)
+    # TODO: if resampling we needs also to act on the mel bands to avoid the part
+    # without signal (upper frequencies)
+    
     mel_extractor = torchaudio.transforms.MelSpectrogram(
         sample_rate=sample_rate, n_fft=n_fft,
         # TODO: Make sure we work with 1sec lengths
-        hop_length=int(1842 / 192000 * sample_rate), n_mels=ntot,
+        #hop_length=int(1842 / 192000 * sample_rate), 
+        hop_length=int(sample_rate/100), 
+        n_mels=ntot,
         win_length=n_fft, power=1)
     mel_extractor = mel_extractor.to(data.device)
     log_mel = torch.log(mel_extractor(data) + .1).T
