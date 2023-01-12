cf5a4f3ad633d942a46256d92ddff7d372c3615d
Bamdad Sabbagh <hi@bamdad.fr>
2023-01-12 15:44:38 +0100

    fix(Bin/ArchiveExamples): Move back to parent directory after archiving a folder

--- bin/archive-examples.sh
+++ bin/archive-examples.sh
@@ -8,12 +8,14 @@ function archive_sse_web_docker_linux(){
   cd sse-web-docker-linux || exit 1
   zip sse-web-docker-linux.zip ./*
   mv sse-web-docker-linux.zip ..
+  cd ..
 }
 
 function archive_sse_web_docker_windows(){
   cd sse-web-docker-windows || exit 1
   zip sse-web-docker-windows.zip ./*
   mv sse-web-docker-windows.zip ..
+  cd ..
 }
 
 browse_to_examples
