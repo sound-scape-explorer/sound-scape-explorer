e6c8e7f78c8b074a4aae701fa883c0b782f48428
Bamdad Sabbagh <hi@bamdad.fr>
2023-01-13 18:24:25 +0100

    feat(Processing/Indicators): Add computation for `NDSI`

--- processing/processing/classes/BuilderIndicatorNormalizedDifferenceSoundscapeIndex.py
+++ processing/processing/classes/BuilderIndicatorNormalizedDifferenceSoundscapeIndex.py
@@ -1,4 +1,5 @@
-from numpy import ndarray
+import maad
+import numpy
 
 from processing.base.BaseBuilderIndicator import BaseBuilderIndicator
 from processing.enum.Indicator import Indicator
@@ -11,5 +12,19 @@ class BuilderIndicatorNormalizedDifferenceSoundscapeIndex(BaseBuilderIndicator):
             self.__processor,
         )
 
-    def __processor(self, sound: ndarray) -> float:
-        return sound[0]
+    def __processor(
+        self,
+        sound: numpy.ndarray,
+        sample_rate: int,
+    ) -> float:
+        spectrogram, tn, fn, ext = maad.sound.spectrogram(sound, sample_rate)
+
+        # noinspection PyPep8Naming
+        NDSI, _, _, _ = maad.features.soundscape_index(
+            spectrogram,
+            fn
+        )
+
+        print('NDSI Soundecology : %2.2f ' % NDSI)
+
+        return NDSI
