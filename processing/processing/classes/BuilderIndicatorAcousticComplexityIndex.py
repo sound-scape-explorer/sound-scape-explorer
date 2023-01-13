5bafea90247d61d690b3adde21852be9b65a8e43
Bamdad Sabbagh <hi@bamdad.fr>
2023-01-13 18:03:00 +0100

    feat(Processing/Indicators): Add computation for `ACI`

--- processing/processing/classes/BuilderIndicatorAcousticComplexityIndex.py
+++ processing/processing/classes/BuilderIndicatorAcousticComplexityIndex.py
@@ -1,4 +1,5 @@
-from numpy import ndarray
+import maad
+import numpy
 
 from processing.base.BaseBuilderIndicator import BaseBuilderIndicator
 from processing.enum.Indicator import Indicator
@@ -11,5 +12,18 @@ class BuilderIndicatorAcousticComplexityIndex(BaseBuilderIndicator):
             self.__processor,
         )
 
-    def __processor(self, sound: ndarray) -> float:
-        return sound[0]
+    def __processor(
+        self,
+        sound: numpy.ndarray,
+        sample_rate: int,
+    ) -> float:
+        spectrogram, tn, fn, ext = maad.sound.spectrogram(
+            sound,
+            sample_rate,
+            mode='amplitude'
+        )
+
+        # noinspection PyPep8Naming
+        _, _, ACI = maad.features.acoustic_complexity_index(spectrogram)
+
+        return ACI
