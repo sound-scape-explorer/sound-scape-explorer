6075f4057e63d33a33ad7c353a4da4f7c48a6ea6
Bamdad Sabbagh <hi@bamdad.fr>
2023-01-13 18:22:30 +0100

    feat(Processing/Indicators): Add computation for `Hf`

--- processing/processing/classes/BuilderIndicatorFrequencyEntropy.py
+++ processing/processing/classes/BuilderIndicatorFrequencyEntropy.py
@@ -1,4 +1,5 @@
-from numpy import ndarray
+import maad
+import numpy
 
 from processing.base.BaseBuilderIndicator import BaseBuilderIndicator
 from processing.enum.Indicator import Indicator
@@ -11,5 +12,21 @@ class BuilderIndicatorFrequencyEntropy(BaseBuilderIndicator):
             self.__processor,
         )
 
-    def __processor(self, sound: ndarray) -> float:
-        return sound[0]
+    def __processor(
+        self,
+        sound: numpy.ndarray,
+        sample_rate: int,
+    ) -> float:
+        spectrogram, _, _, _ = maad.sound.spectrogram(sound, sample_rate)
+
+        frequency_entropy, frequency_entropy_per_bin = \
+            maad.features.frequency_entropy(
+                spectrogram
+            )
+
+        print(frequency_entropy)
+        print(
+            'Length of Ht_per_bin is : %2.0f' % len(frequency_entropy_per_bin)
+        )
+
+        return frequency_entropy
