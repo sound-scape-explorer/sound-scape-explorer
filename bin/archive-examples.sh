9376fdb426818e27f1e8637c768ec156ec9808a9
Bamdad Sabbagh <hi@bamdad.fr>
2023-01-18 11:37:09 +0100

    feat(Examples): Add `SSE Next Docker for Windows` version

--- bin/archive-examples.sh
+++ bin/archive-examples.sh
@@ -11,21 +11,29 @@ function archive_sse_docker_windows(){
   cd ..
 }
 
+function archive_sse_next_docker_windows(){
+  cd sse-next-docker-windows || exit 1
+  zip sse-next-docker-windows.zip ./* -x "*.gitignore"
+  mv sse-next-docker-windows.zip ..
+  cd ..
+}
+
 function archive_sse_web_docker_linux(){
   cd sse-web-docker-linux || exit 1
-  zip sse-web-docker-linux.zip ./*
+  zip sse-web-docker-linux.zip ./* -x "*.gitignore"
   mv sse-web-docker-linux.zip ..
   cd ..
 }
 
 function archive_sse_web_docker_windows(){
   cd sse-web-docker-windows || exit 1
-  zip sse-web-docker-windows.zip ./*
+  zip sse-web-docker-windows.zip ./* -x "*.gitignore"
   mv sse-web-docker-windows.zip ..
   cd ..
 }
 
 browse_to_examples
 archive_sse_docker_windows
+archive_sse_next_docker_windows
 archive_sse_web_docker_linux
 archive_sse_web_docker_windows
---
+++ examples/sse-next-docker-windows/start.ps1
@@ -0,0 +1,36 @@
+function PrintSeparator()
+{
+    echo "---"
+}
+
+function PrintWelcome()
+{
+    PrintSeparator
+    echo "Welcome to SSE Next Docker for Windows."
+    echo "Press CTRL+C to stop the app and exit."
+    PrintSeparator
+}
+
+function PrintUpdate()
+{
+    PrintSeparator
+    echo "Updating docker images..."
+    PrintSeparator
+}
+
+function PrintStart()
+{
+    PrintSeparator
+    echo "Starting docker containers..."
+    PrintSeparator
+}
+
+# Runtime
+
+PrintWelcome
+
+PrintUpdate
+docker compose pull
+
+PrintStart
+docker compose up
