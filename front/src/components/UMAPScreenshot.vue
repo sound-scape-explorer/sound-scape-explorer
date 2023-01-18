acb34070d160cd0b84323a2d7b14810d286329c1
Bamdad Sabbagh <hi@bamdad.fr>
2023-01-18 19:13:01 +0100

    fix(Front/UMAP): Screenshot component now correctly disables on UMAP status

--- front/src/components/UMAPScreenshot.vue
+++ front/src/components/UMAPScreenshot.vue
@@ -1,12 +1,11 @@
 <script lang="ts" setup="">
 import Button from './Button.vue';
-import {selectionImageStore} from '../store/selection-image.store';
-import {computed} from 'vue';
 import {ImageOutline} from '@vicons/ionicons5';
 import {useUMAPComponent} from '../composables/useUMAPComponent';
+import {useUMAPStatus} from '../composables/useUMAPStatus';
 
 const {screenshot} = useUMAPComponent();
-const isDisabled = computed<boolean>(() => selectionImageStore.image === null);
+const {isDisabled} = useUMAPStatus();
 </script>
 
 <template>
