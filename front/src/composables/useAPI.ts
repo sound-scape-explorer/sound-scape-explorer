c5174ef39c5ea5fd2e8b1258d617301be4a5e3b0
Bamdad Sabbagh <hi@bamdad.fr>
2023-01-23 16:40:08 +0100

    feat(Front/UMAP): Notify user when data can not be fetched

---
+++ front/src/composables/useAPI.ts
@@ -0,0 +1,23 @@
+import {API_ROUTES} from '../constants';
+import type {ApiUMAPInterface} from '../interfaces/api-UMAP.interface';
+import {useNotification} from './useNotification';
+
+export function useAPI() {
+  const {notify} = useNotification();
+
+  async function fetchUMAP(interval: string, band: string): Promise<ApiUMAPInterface | void> {
+    const endpoint = API_ROUTES.umap({interval, band});
+    const request = await fetch(endpoint);
+
+    if (!request.ok) {
+      notify('error', 'UMAP data could not be fetched!', `interval ${interval}, band ${band}`);
+      return;
+    }
+
+    return request.json();
+  }
+
+  return {
+    fetchUMAP,
+  };
+}
