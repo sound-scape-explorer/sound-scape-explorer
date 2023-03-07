cd4b6a158ddfb6e61cf4b074278015fefeb20a01
Bamdad Sabbagh <hi@bamdad.fr>
2023-03-08 00:09:36 +0100

    fix(Front/Scatter): Correctly create scatterGL dataset

--- front/src/utils/convert-to-scatter-gl-dataset.ts
+++ front/src/utils/convert-to-scatter-gl-dataset.ts
@@ -1,6 +1,5 @@
 import type {Point2D, PointMetadata} from '../lib/scatter-gl-0.0.13';
 import {ScatterGL} from '../lib/scatter-gl-0.0.13';
-import {getIntegratedIndex} from './get-integrated-index';
 
 interface MyMetadata {
   labelIndex: number;
@@ -20,23 +19,28 @@ interface Props {
 export function convertToScatterGlDataset(props: Props) {
   const dataPoints: Point2D[] = props.features.flat() as unknown as Point2D[];
   const metadata: MyMetadata[] = [];
+  const integrations = props.features.flat().length / props.features.length;
+  let count = 0;
 
-  for (let i = 0; i < dataPoints.length; i += 1) {
-    const integratedIndex = getIntegratedIndex(i, props.files.length);
+  for (let i = 0; i < props.files.length; i += 1) {
+    const label = props.files[i];
+    const metaValues = props.metas[i];
 
-    const label = props.files[integratedIndex];
-    const labelIndex = i;
-    const timestamp = props.timestamps[i];
-    const tags = '';
-    const metaValues = props.metas[integratedIndex];
+    for (let j = 0; j < integrations; j += 1) {
+      const labelIndex = count;
+      const timestamp = props.timestamps[labelIndex];
+      const tags = '';
 
-    metadata.push({
-      label,
-      labelIndex,
-      timestamp,
-      tags,
-      metaValues,
-    });
+      metadata.push({
+        label,
+        labelIndex,
+        timestamp,
+        tags,
+        metaValues,
+      });
+
+      count += 1;
+    }
   }
 
   return new ScatterGL.Dataset(dataPoints, metadata as unknown as PointMetadata[]);
