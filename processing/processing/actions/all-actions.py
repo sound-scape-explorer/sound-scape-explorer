7800e6131d9449f425f7ff3eef76685d9cfaf55a
Bamdad Sabbagh <hi@bamdad.fr>
2023-03-09 10:48:40 +0100

    feat(Processing): Add command and action `all-actions`

---
+++ processing/processing/actions/all-actions.py
@@ -0,0 +1,6 @@
+# noinspection PyUnresolvedReferences
+import processing.actions.indicators
+# noinspection PyUnresolvedReferences
+import processing.actions.reducers
+# noinspection PyUnresolvedReferences
+import processing.actions.volumes
