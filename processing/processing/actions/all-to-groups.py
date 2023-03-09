9280725317dac9f637a0349de064ce8b14171726
Bamdad Sabbagh <hi@bamdad.fr>
2023-03-09 01:11:24 +0100

    feat(Processing): Add command and action `all-to-groups`

---
+++ processing/processing/actions/all-to-groups.py
@@ -0,0 +1,6 @@
+# noinspection PyUnresolvedReferences
+import processing.actions.config
+# noinspection PyUnresolvedReferences
+import processing.actions.files
+# noinspection PyUnresolvedReferences
+import processing.actions.groups
