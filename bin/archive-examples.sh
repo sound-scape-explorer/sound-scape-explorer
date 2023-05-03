c280f4093670383ea47480dc7b4b597389e9bcc0
Bamdad Sabbagh <hi@bamdad.fr>
2023-05-03 20:29:50 +0200

    chore(Bin): Apply bash formatting

--- bin/archive-examples.sh
+++ bin/archive-examples.sh
@@ -2,33 +2,32 @@
 
 # Functions
 
-function iterate(){
-  local name
+function iterate() {
+	local name
 
-  function archive(){
-    cd "$name" || exit 1
-    cp ../common/start-windows.ps1 . && wait
-    cp ../common/start-linux.sh . && wait
-    cp ../common/project.env . && wait
-    zip "$name.zip" ./* -r -x "*.gitignore" "*.png"
-    mv "$name.zip" ..
-    cd ..
-  }
+	function archive() {
+		cd "$name" || exit 1
+		cp ../common/start-windows.ps1 . && wait
+		cp ../common/start-linux.sh . && wait
+		cp ../common/project.env . && wait
+		zip "$name.zip" ./* -r -x "*.gitignore" "*.png"
+		mv "$name.zip" ..
+		cd ..
+	}
 
-  for value in "${names[@]}"
-  do
-    name="${value}"
-    archive
-  done
+	for value in "${names[@]}"; do
+		name="${value}"
+		archive
+	done
 }
 
 # Main
 
 names=(
-  sse-cpu-docker
-  sse-cuda-docker
-  sse-front-docker
-  sse-next-docker
+	sse-cpu-docker
+	sse-cuda-docker
+	sse-front-docker
+	sse-next-docker
 )
 
 cd examples || exit 1
