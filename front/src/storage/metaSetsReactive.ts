9b978e1402d32cc8fee0555b91c355fdfcdcbaab
Bamdad Sabbagh <hi@bamdad.fr>
2023-05-11 22:48:21 +0200

    chore(Front): Remove dead code

--- front/src/storage/metaSetsReactive.ts
+++
@@ -1,9 +0,0 @@
-import {reactive} from 'vue';
-
-export interface MetaSetsReactive {
-  data: null | string[][];
-}
-
-export const metaSetsReactive = reactive<MetaSetsReactive>({
-  data: null,
-});
