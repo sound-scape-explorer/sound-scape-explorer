735eed13424575278d1c42ce1d1a7bf0c4edebb0
Bamdad Sabbagh <hi@bamdad.fr>
2023-05-25 16:20:51 +0200

    perf(Front/Colors): Use new dataset readiness reference + Remove deprecated `useScatterStatus` hook

--- front/src/components/Colors/ColorsAlphas.vue
+++ front/src/components/Colors/ColorsAlphas.vue
@@ -2,11 +2,11 @@
 import type {InputNumberProps} from 'naive-ui';
 import {NInputNumber, NTooltip} from 'naive-ui';
 import {scatterAlphasStore} from '../Scatter/scatterStore';
-import {useScatterStatus} from '../Scatter/useScatterStatus';
+import {isDatasetReadyRef} from '../Scatter/useScatterDataset';
 
-const {isDisabled} = useScatterStatus();
-
-type InputNumberThemeOverrides = NonNullable<InputNumberProps['themeOverrides']>
+type InputNumberThemeOverrides = NonNullable<
+  InputNumberProps['themeOverrides']
+>;
 
 const inputNumberThemeOverrides: InputNumberThemeOverrides = {
   peers: {
@@ -18,11 +18,14 @@ const inputNumberThemeOverrides: InputNumberThemeOverrides = {
 </script>
 
 <template>
-  <n-tooltip placement="right" trigger="hover">
+  <n-tooltip
+    placement="right"
+    trigger="hover"
+  >
     <template #trigger>
       <n-input-number
         v-model:value="scatterAlphasStore.low"
-        :disabled="isDisabled"
+        :disabled="!isDatasetReadyRef.value"
         :theme-overrides="inputNumberThemeOverrides"
         class="input"
         max="1"
@@ -34,11 +37,14 @@ const inputNumberThemeOverrides: InputNumberThemeOverrides = {
     <span>Opacity for excluded points</span>
   </n-tooltip>
 
-  <n-tooltip placement="right" trigger="hover">
+  <n-tooltip
+    placement="right"
+    trigger="hover"
+  >
     <template #trigger>
       <n-input-number
         v-model:value="scatterAlphasStore.high"
-        :disabled="isDisabled"
+        :disabled="!isDatasetReadyRef.value"
         :theme-overrides="inputNumberThemeOverrides"
         class="input"
         max="1"
@@ -49,7 +55,6 @@ const inputNumberThemeOverrides: InputNumberThemeOverrides = {
     </template>
     <span>Opacity for collected points</span>
   </n-tooltip>
-
 </template>
 
 <style lang="scss" scoped>
--- front/src/components/Colors/ColorsScale.vue
+++ front/src/components/Colors/ColorsScale.vue
@@ -2,27 +2,24 @@
 import {NSelect, NTooltip} from 'naive-ui';
 import {computed} from 'vue';
 import {convertToNaiveSelectOptions} from '../../utils/convert-to-naive-select-options';
-import {useScatterStatus} from '../Scatter/useScatterStatus';
 import {colorsStore} from './colorsStore';
+import {isDatasetReadyRef} from '../Scatter/useScatterDataset';
 
-const {isDisabled} = useScatterStatus();
-
-const options = [
-  'Accent',
-  'Dark2',
-  'Spectral',
-];
+const options = ['Accent', 'Dark2', 'Spectral'];
 
 const naiveOptions = computed(() => convertToNaiveSelectOptions(options));
 </script>
 
 <template>
-  <n-tooltip placement="right" trigger="hover">
+  <n-tooltip
+    placement="right"
+    trigger="hover"
+  >
     <template #trigger>
       <n-select
         v-model:value="colorsStore.colorScale"
         :default-value="options[1]"
-        :disabled="isDisabled"
+        :disabled="!isDatasetReadyRef.value"
         :options="naiveOptions"
         placeholder="Color scale..."
         size="small"
