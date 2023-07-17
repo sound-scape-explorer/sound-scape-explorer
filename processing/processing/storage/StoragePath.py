696812840d088026c93c9761678369cffecb52c4
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-17 22:43:39 +0200

    docs(Processing/Storage): Improve documentation

--- processing/processing/storage/StoragePath.py
+++ processing/processing/storage/StoragePath.py
@@ -4,10 +4,11 @@ from enum import Enum
 class StoragePath(Enum):
     """The paths for all data located in the storage file.
 
-    File type: HDF
+    File type:
+        HDF
 
-    See details at
-    https://www.wikiwand.com/en/Hierarchical_Data_Format
+    Details:
+        https://www.wikiwand.com/en/Hierarchical_Data_Format
     """
 
     #################
@@ -23,19 +24,24 @@ class StoragePath(Enum):
     User will need to refresh using `pnpm process:config` to update data located
     inside the storage file.
 
-    Type: Group
+    Type:
+        Group
     """
 
     #######################
     # Configuration/Bands #
     #######################
 
+    # INFO: This allows duplicates that can cause collisions because bands
+    # are referenced by their names and not absolute indexes.
+    # A solution would be to reference by index or force user to have unique names.
     bands = "/configuration/bands/names"
-    """The enumeration of requested bands.
+    """The names of specified bands.
 
     Each row contains a string.
 
-    Type: Dataset
+    Type:
+        Dataset
     """
 
     bands_frequencies = "/configuration/bands/frequencies"
@@ -43,27 +49,78 @@ class StoragePath(Enum):
 
     Each row contains 2 columns for each integer (low and high frequency in Hertz).
 
-    Type: Dataset
+    Type:
+        Dataset
     """
 
     ##############################
     # Configuration/Integrations #
     ##############################
 
+    # INFO: This allows duplicates thus can cause collisions.
+    # See `Configuration/Bands` annotation for more details.
     integrations = "/configuration/integrations/names"
+    """The names of specified integrations.
+
+    Each row contains a string.
+
+    Type:
+        Dataset
+    """
+
     integrations_seconds = "/configuration/integrations/seconds"
+    """The values in seconds for each band.
 
-    # Ranges
+    Each row contains an integer.
 
+    Type:
+        Dataset
+    """
+
+    ########################
+    # Configuration/Ranges #
+    ########################
+
+    # INFO: This allows duplicates thus can cause collisions.
+    # See `Configuration/Bands` annotation for more details.
     ranges = "/configuration/ranges/names"
+    """The names of specified ranges.
+
+    Each row contains a string.
+
+    Type:
+        Dataset
+    """
+
     ranges_timestamps = "/configuration/ranges/timestamps"
+    """The timestamps for each range.
+
+    Each row contains two columns of integers.
+    The first column is the start date.
+    The second column is the end date.
+
+    Each date is in `milliseconds` in UNIX format.
+
+    Type:
+        Dataset
+    """
 
     #########
     # Files #
     #########
 
     files = "/configuration/files/names"
-    """Example: /files_features/{band}/{file_index}
+    """The file paths relative to the audio folder.
+
+    Each row contains a string.
+    Each string starts with a `/`.
+    This `/` is a convention because it gets merged with the audio folder path.
+
+    Example:
+        /files_features/{band}/{file_index}
+
+    Type:
+        Dataset
     """
 
     files_sites = "/configuration/files/sites"
@@ -88,7 +145,7 @@ class StoragePath(Enum):
     """
 
     ################
-    # Autoclusters #
+    # Configuration/Autoclusters #
     ################
 
     autoclusters_names = "/configuration/autoclusters/names"
@@ -143,16 +200,40 @@ class StoragePath(Enum):
     Each dataset is named after the integration value.
     Each row represents how many groups (integrations) have been processed.
 
-    Example: /files_groups_count/{INTEGRATION}
+    Example: /files_groups_count/{integration}
     """
 
-    ############
-    # Reducers #
-    ############
+    ##########################
+    # Configuration/Reducers #
+    ##########################
 
     reducers = "/configuration/reducers/names"
+    """The names of specified reducers.
+
+    Each row contains a string.
+
+    See enumeration at:
+        ../reducers/ReducerName.py
+
+    Type: Dataset
+    """
+
     reducers_dimensions = "/configuration/reducers/dimensions"
+    """The dimensions for each reducer.
+
+    Each row contains an integer.
+
+    Type: Dataset
+    """
+
     reducers_bands = "/configuration/reducers/bands"
+    """The bands for each reducer.
+
+    Each row contains a string corresponding to the name of the band.
+
+    Type: Dataset
+    """
+
     reducers_integrations = "/configuration/reducers/integrations"
     reducers_ranges = "/configuration/reducers/ranges"
 
