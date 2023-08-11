f57a0044166eb2e1ad600e4ed03c36f69b0df5e4
Bamdad Sabbagh <hi@bamdad.fr>
2023-08-12 00:15:47 +0200

    feat(Processing/Actions): Ask user to replace old storage file with new repacked one

--- processing/processing/actions/run_repack.py
+++ processing/processing/actions/run_repack.py
@@ -4,12 +4,16 @@ from rich import print
 
 from processing.interfaces import IMain
 from processing.storage.Storage import Storage
+from processing.utils.ask_for_repack_replacement import ask_for_repack_replacement
 
 
 def run_repack(
     storage: Storage,
     callback: IMain,
 ):
+    # Need to close the storage first
+    storage.close()
+
     path_repack = storage.path.replace(".h5", ".repack.h5")
 
     subprocess.run(
@@ -20,5 +24,18 @@ def run_repack(
         ]
     )
 
-    print(f":rocket: Storage file repacked to {path_repack}")
-    callback(storage)
+    print(f"Repacked storage location: {path_repack}")
+    print("[bold green]:rocket: Storage file repack completed![/bold green]")
+
+    answer = ask_for_repack_replacement()
+
+    if answer is True:
+        path_old = storage.path
+        subprocess.run(["rm", path_old])
+        subprocess.run(["mv", path_repack, path_old])
+
+        storage_repacked = Storage(path_old)
+        callback(storage_repacked)
+    else:
+        storage.load()
+        callback(storage)
---
+++ processing/processing/utils/ask_for_repack_replacement.py
@@ -0,0 +1,18 @@
+from PyInquirer import prompt
+from rich import print
+
+
+def ask_for_repack_replacement() -> bool:
+    questions = [
+        {
+            "type": "confirm",
+            "message": "Do you want to replace your old storage file?",
+            "name": "replace",
+            "default": False,
+        },
+    ]
+
+    print()
+    answers = prompt(questions)
+    answer: bool = answers["replace"]
+    return answer
