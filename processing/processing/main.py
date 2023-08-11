31a7ca21628b829999df2dd8ed63e00f2430ec1c
Bamdad Sabbagh <hi@bamdad.fr>
2023-08-11 20:39:29 +0200

    feat(Processing): Wrap application in traceback pretty printer

--- processing/processing/main.py
+++ processing/processing/main.py
@@ -1,8 +1,11 @@
 from signal import SIGINT, signal
 from typing import Optional
 
+from rich.console import Console
+
 from processing.actions.run_config import run_config
 from processing.actions.run_extractions import run_extractions
+from processing.actions.run_reductions import run_reductions
 from processing.actions.run_repack import run_repack
 from processing.storage.Storage import Storage
 from processing.utils.ask_menu import MenuChoice, ask_menu
@@ -16,28 +19,35 @@ def main(
     loaded_storage: Optional[Storage] = None,
 ):
     """CLI entry point"""
-    update_python_path()
 
-    env = get_yaml_data()
+    try:
+        update_python_path()
 
-    if loaded_storage is None:
-        print_yaml_env(env)
-        storage = Storage(env.storage)
-    else:
-        storage = loaded_storage
+        env = get_yaml_data()
 
-    signal(SIGINT, lambda _signum, _frame: quit_sse(storage))
+        if loaded_storage is None:
+            print_yaml_env(env)
+            storage = Storage(env.storage)
+        else:
+            storage = loaded_storage
 
-    answer = ask_menu()
+        signal(SIGINT, lambda _signum, _frame: quit_sse(storage))
 
-    if answer == MenuChoice.RefreshConfig.value:
-        run_config(env, storage, main)
-    if answer == MenuChoice.ExtractAggregate.value:
-        run_extractions(storage, main)
-    if answer == MenuChoice.Repack.value:
-        run_repack(storage, main)
-    else:
-        quit_sse(storage)
+        answer = ask_menu()
+
+        if answer == MenuChoice.RefreshConfig.value:
+            run_config(env, storage, main)
+        if answer == MenuChoice.ExtractAggregate.value:
+            run_extractions(storage, main)
+        if answer == MenuChoice.Reduce.value:
+            run_reductions(storage, main)
+        if answer == MenuChoice.Repack.value:
+            run_repack(storage, main)
+        else:
+            quit_sse(storage)
+    except Exception:
+        console = Console()
+        console.print_exception(show_locals=True)
 
 
 if __name__ == "__main__":
