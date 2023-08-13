8490c14050ab1ebf3cad339351cb11bedcf95763
Bamdad Sabbagh <hi@bamdad.fr>
2023-08-13 03:41:18 +0200

    feat(Processing/Storage): Add path getters for `AggregatedReduceable`

--- processing/processing/storage/AggregatedReduceable.py
+++ processing/processing/storage/AggregatedReduceable.py
@@ -3,6 +3,7 @@ from h5py import Dataset
 from processing.config.bands.BandConfig import BandConfig
 from processing.config.extractors.ExtractorConfig import ExtractorConfig
 from processing.config.integrations.IntegrationConfig import IntegrationConfig
+from processing.config.reducers.ReducerConfig import ReducerConfig
 from processing.storage.Storage import Storage
 from processing.storage.StoragePath import StoragePath
 
@@ -30,3 +31,20 @@ class AggregatedReduceable:
 
     def exists_in_storage(self, storage: Storage) -> bool:
         return storage.exists_dataset(self.path)
+
+    def get_timestamps_path(self) -> str:
+        return (
+            f"{StoragePath.aggregated_timestamps.value}"
+            f"/{self.band.name}"
+            f"/{self.integration.seconds}"
+            f"/{self.extractor.index}"
+        )
+
+    def get_reduced_path(self, reducer: ReducerConfig) -> str:
+        return (
+            f"{StoragePath.reduced.value}"
+            f"/{self.band.name}"
+            f"/{self.integration.seconds}"
+            f"/{self.extractor.index}"
+            f"/{reducer.index}"
+        )
