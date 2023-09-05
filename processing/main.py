049a6ea4af9edc2e3809f930587b02c9027deedc
Bamdad Sabbagh <hi@bamdad.fr>
2023-09-05 20:15:31 +0200

    feat(Processing): Instanciate app correctly (checking PYTHONPATH before imports)

---
+++ processing/main.py
@@ -0,0 +1,29 @@
+import argparse
+import os
+import platform
+import sys
+
+from processing.main import main
+
+
+def update_python_path():
+    current_path = os.getcwd()
+
+    if platform.system() == "Windows":
+        processing_path = f"{current_path}\\processing"
+    else:
+        processing_path = f"{current_path}/processing"
+
+    # Append `processing` path to PYTHONPATH
+    if processing_path not in sys.path:
+        sys.path.append(processing_path)
+
+
+if __name__ == "__main__":
+    update_python_path()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("yaml_path")
+    args = parser.parse_args()
+
+    main(yaml_path=args.yaml_path)
