7f4c62d56d0118132445bde8085b318d74450a26
Bamdad Sabbagh <hi@bamdad.fr>
2023-10-20 17:46:46 +0200

    fix(Processing): Update default dicts

--- processing/dicts.py
+++ processing/dicts.py
@@ -1,7 +1,8 @@
+import numpy
 from torch import hub
 
 hub.load_state_dict_from_url(
-    'https://github.com/harritaylor/torchvggish/'
-    'releases/download/v0.1/vggish-10086976.pth',
-    progress=True
+    "https://github.com/harritaylor/torchvggish/"
+    "releases/download/v0.1/vggish-10086976.pth",
+    progress=True,
 )
