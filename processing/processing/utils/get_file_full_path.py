a8693aa88e2b806dcc6ec7b5f5baae71316c3f29
Bamdad Sabbagh <hi@bamdad.fr>
2023-10-26 20:20:11 +0200

    fix(Processing/Files): Fix typo in full path helper

--- processing/processing/utils/get_file_full_path.py
+++ processing/processing/utils/get_file_full_path.py
@@ -2,7 +2,7 @@ from processing.utils.print_action import print_action
 
 
 def get_file_full_path(filename: str, audio_folder: str):
-    if filename == "/":
+    if filename[0] == "/":
         path = f"{audio_folder}{filename}"
     else:
         print_action(f"File name {filename} does not start with a /", "error")
