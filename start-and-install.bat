db5002dd37ee851981e1ca26626329f4341b39b1
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-02 13:48:26 +0100

    perf(Processing): Add starter windows batch script with auto install of visualisation modules

---
+++ start-and-install.bat
@@ -0,0 +1,16 @@
+@echo off
+
+if not exist "venv\Scripts\Activate.ps1" (
+    echo Creating virtual environment...
+    python3.10 -m venv venv
+
+    echo
+    echo Installing visualisation packages...
+    pnpm i
+    pnpm install:front
+
+    echo
+    echo Please now install processing module packages manually...
+)
+
+start /B powershell.exe -ExecutionPolicy Bypass -NoExit -Command "& 'venv\Scripts\Activate.ps1'"
