99338d7f4f1aea56d7e1048722cd657a1134f3fd
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-08 12:03:26 +0100

    fix(Processing/Config): Add checks for labels declared in trajectory tab

--- processing/processing/config/trajectories/TrajectoryStorage.py
+++ processing/processing/config/trajectories/TrajectoryStorage.py
@@ -4,12 +4,14 @@ from pandas import Timestamp
 
 from processing.config.ConfigParser import ConfigParser
 from processing.config.ExcelSheet import ExcelSheet
+from processing.config.labels.LabelConfig import LabelConfig
 from processing.config.trajectories.TrajectoryConfig import TrajectoryConfig
 from processing.config.trajectories.TrajectoryExcel import TrajectoryExcel
 from processing.storage.Storage import Storage
 from processing.storage.StoragePath import StoragePath
 from processing.utils.convert_date_to_timestamp import convert_date_to_timestamp
 from processing.utils.validate_excel_names import validate_excel_names
+from processing.utils.validate_trajectory_labels import validate_trajectory_labels
 
 
 class TrajectoryStorage:
@@ -66,7 +68,10 @@ class TrajectoryStorage:
         return trajectories
 
     @staticmethod
-    def read_from_config(parser: ConfigParser) -> List[TrajectoryConfig]:
+    def read_from_config(
+        parser: ConfigParser,
+        labels: List[LabelConfig],
+    ) -> List[TrajectoryConfig]:
         sheet = ExcelSheet.trajectories
 
         names = parser.get(sheet, TrajectoryExcel.name_)
@@ -82,6 +87,8 @@ class TrajectoryStorage:
         label_values = parser.get(sheet, TrajectoryExcel.label_value)
         step_names = parser.get(sheet, TrajectoryExcel.step)
         steps = [TrajectoryConfig.step_by_name[sn] for sn in step_names]
+
+        validate_trajectory_labels(names, label_properties, label_values, labels)
 
         trajectories = TrajectoryConfig.reconstruct(
             names=names,
