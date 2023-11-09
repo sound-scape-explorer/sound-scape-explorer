3675fecd175625d1b1471c2f2f26384511f2c8c3
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-09 16:19:47 +0100

    fix(Processing/CLI): Feed absolute audio path to visualisation command

--- processing/processing/utils/read_audio_path_from_config.py
+++ processing/processing/utils/read_audio_path_from_config.py
@@ -2,10 +2,11 @@ import os
 
 from processing.config.ConfigParser import ConfigParser
 from processing.config.settings.SettingsStorage import SettingsStorage
+from processing.utils.get_absolute_path import get_absolute_path
 
 
 def read_audio_path_from_config(config_path: str) -> str:
     folder_path = os.path.dirname(config_path)
     parser = ConfigParser(config_path, folder_path)
     settings = SettingsStorage.read_from_config(parser)
-    return settings.audio_path
+    return get_absolute_path(settings.audio_path)
