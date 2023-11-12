308a8bd6851dbd82841bd1133ac34cba489aa45c
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-12 01:18:56 +0100

    chore(Front): Update code style for `App.vue`

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,22 @@
 <script lang="ts" setup>
-import {NLayout, NNotificationProvider, NSpace} from 'naive-ui';
 import 'sass-reset';
+
+import {NLayout, NNotificationProvider, NSpace} from 'naive-ui';
+
 import AppNotification from './components/AppNotification/AppNotification.vue';
 import Loading from './components/Loading/Loading.vue';
 import Explore from './pages/Explore.vue';
 </script>
 
 <template>
-  <n-space size="large" vertical>
-    <n-layout has-sider sider-placement="left">
+  <n-space
+    size="large"
+    vertical
+  >
+    <n-layout
+      has-sider
+      sider-placement="left"
+    >
       <Loading />
 
       <n-layout>
