65a5ef13f1af93460116b67a144b95dc73b0ee86
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-28 02:30:46 +0100

    feat(Processing/Menu): Improve menu welcome message

--- processing/processing/utils/print_welcome.py
+++ processing/processing/utils/print_welcome.py
@@ -1,6 +1,16 @@
 from rich import print
 
+from processing.constants import APP_NAME
+from processing.utils.get_version_from_setup import get_version_from_setup
+
 
 def print_welcome() -> None:
-    print("[bold green]:waving_hand: Welcome to SoundScape Explorer![/bold green]")
+    version = get_version_from_setup()
+    print(
+        f"[green]"
+        f":waving_hand: Welcome to [bold]{APP_NAME}[/bold]"
+        f" "
+        f"[italic]v{version}[/italic]"
+        f"[/green]"
+    )
     print()
