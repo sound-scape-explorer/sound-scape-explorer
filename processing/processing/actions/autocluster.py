9e6a24b6b88734853111c7687e50ccfdbfa35517
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-29 22:49:02 +0100

    perf(Processing/Autocluster): Extract write to storage

--- processing/processing/actions/autocluster.py
+++ processing/processing/actions/autocluster.py
@@ -5,7 +5,6 @@ from processing.config.bands.BandStorage import BandStorage
 from processing.config.integrations.IntegrationStorage import IntegrationStorage
 from processing.interfaces import MenuCallback
 from processing.storage.Storage import Storage
-from processing.storage.StoragePath import StoragePath
 from processing.utils.invoke_menu import invoke_menu
 from processing.utils.print_action import print_action
 from processing.utils.print_autoclusters import print_autoclusters
@@ -40,29 +39,7 @@ def autocluster(
             ac.create_instance(band, integration)
             mdm = storage.read(MeanDistancesMatrix.get_path(band, integration))
             ac.calculate(mdm[:])
-
-            path = (
-                f"{StoragePath.autoclustered.value}"
-                f"/{band.name}"
-                f"/{integration.seconds}"
-                f"/{ac.index}"
-            )
-
-            attributes = {
-                "min_cluster_size": ac.min_cluster_size,
-                "min_samples": ac.min_samples,
-                "alpha": ac.alpha,
-                "epsilon": ac.epsilon,
-                "name": ac.name,
-                "index": ac.index,
-            }
-
-            storage.write(
-                path=path,
-                data=ac.values,
-                compression=True,
-                attributes=attributes,
-            )
+            AutoclusteredStorage.write(storage, band, integration, ac)
 
     print_action("Autoclustering completed!", "end")
     invoke_menu(storage, callback)
--- processing/processing/common/AutoclusteredStorage.py
+++ processing/processing/common/AutoclusteredStorage.py
@@ -39,3 +39,41 @@ class AutoclusteredStorage:
             autoclustereds.append(autoclustered)
 
         return autoclustereds
+
+    @staticmethod
+    def get_path(
+        band: BandConfig,
+        integration: IntegrationConfig,
+        ac: AutoclusterConfig,
+    ) -> str:
+        return (
+            f"{StoragePath.autoclustered.value}"
+            f"/{band.name}"
+            f"/{integration.seconds}"
+            f"/{ac.index}"
+        )
+
+    @staticmethod
+    def write(
+        storage: Storage,
+        band: BandConfig,
+        integration: IntegrationConfig,
+        ac: AutoclusterConfig,
+    ) -> None:
+        path = AutoclusteredStorage.get_path(band, integration, ac)
+
+        attributes = {
+            "min_cluster_size": ac.min_cluster_size,
+            "min_samples": ac.min_samples,
+            "alpha": ac.alpha,
+            "epsilon": ac.epsilon,
+            "name": ac.name,
+            "index": ac.index,
+        }
+
+        storage.write(
+            path=path,
+            data=ac.values,
+            compression=True,
+            attributes=attributes,
+        )
