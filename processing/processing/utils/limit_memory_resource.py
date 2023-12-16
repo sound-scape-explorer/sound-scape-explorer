19cb44b1363a4c1f25e708cd4df799db7152911b
Bamdad Sabbagh <hi@bamdad.fr>
2023-12-16 21:10:39 +0100

    fix(Processing/Utils): Remove misleading default gigabytes ram limit (test)

--- processing/processing/utils/limit_memory_resource.py
+++ processing/processing/utils/limit_memory_resource.py
@@ -1,11 +1,9 @@
 from typing import Union
 
-minimum_ram_in_gigabytes = 5.2
-
 
 # Use for testing purposes to artificially limit the amount of memory used
 def limit_memory_resource(
-    gigabytes: Union[int, float] = minimum_ram_in_gigabytes,
+    gigabytes: Union[int, float],
 ) -> None:
     import resource
 
