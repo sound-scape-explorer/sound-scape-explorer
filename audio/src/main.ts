87ef944aae93bed8b25a283addf280adb6fc920a
Bamdad Sabbagh <hi@bamdad.fr>
2024-01-09 00:43:42 +0100

    feat(Audio): Create API to slice files before sending over network

---
+++ audio/src/main.ts
@@ -0,0 +1,76 @@
+import cors from 'cors';
+import express from 'express';
+import {existsSync} from 'fs';
+import {getAudioDurationInSeconds} from 'get-audio-duration';
+
+import {sliceAudio} from './slice-audio';
+
+const app = express();
+const port = 5531;
+const args = process.argv;
+const audioPath = args[args.length - 1];
+
+app.use(cors());
+
+// service
+app.listen(port, () => {
+  console.log(`port: ${port}`);
+  console.log(`audio path: ${audioPath}`);
+});
+
+// root endpoint
+app.get('/', (_req, res) => {
+  res.send({
+    message: 'hello',
+    version: '1.0.0',
+  });
+});
+
+// get audio endpoint
+// start and end in milliseconds
+app.get('/get', async (req, res) => {
+  const {file, start, end} = req.query;
+  const path = `${audioPath}${file}`;
+
+  // validate parameters
+  if (typeof file === 'undefined' || typeof start === 'undefined' || typeof end === 'undefined') {
+    res.writeHead(403, {'Content-Type': 'text/html'});
+    res.end('Error: Missing parameters');
+    return;
+  }
+
+  // validate file existence
+  if (!existsSync) {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end('Error: File not found');
+    return;
+  }
+
+  const fileDuration = await getAudioDurationInSeconds(path);
+  const startSeconds = Number(start) / 1000;
+  const endSeconds = Number(end) / 1000;
+
+  // validate start and end
+  if (startSeconds < 0 || endSeconds < 0) {
+    res.writeHead(403, {'Content-Type': 'text/html'});
+    res.end('Error: Start or end is less than 0');
+    return;
+  }
+
+  if (startSeconds >= endSeconds) {
+    res.writeHead(403, {'Content-Type': 'text/html'});
+    res.end('Error: Start is greater than end');
+    return;
+  }
+
+  if (startSeconds > fileDuration || endSeconds > fileDuration) {
+    res.writeHead(403, {'Content-Type': 'text/html'});
+    res.end('Error: Start or end is greater than audio duration');
+    return;
+  }
+
+  // slice
+  const slice = await sliceAudio(path, startSeconds, endSeconds);
+  res.writeHead(200, {'Content-Type': 'audio/wav'});
+  res.end(slice);
+});
---
+++ audio/src/slice-audio.ts
@@ -0,0 +1,45 @@
+import {spawn} from 'child_process';
+import {existsSync, readFile, unlinkSync} from 'fs';
+
+const tempFile = 'scratch.wav';
+
+export function sliceAudio(
+  file: string,
+  start: number,
+  end: number,
+): Promise<Buffer> {
+  return new Promise((resolve) => {
+    if (existsSync(tempFile)) {
+      unlinkSync(tempFile);
+    }
+
+    const duration = end - start;
+
+    const ffmpeg = spawn('ffmpeg', [
+      '-i',
+      file,
+      '-acodec',
+      'copy',
+      '-ss',
+      `00:00:${start}`,
+      '-t',
+      `00:00:${duration}`,
+      tempFile,
+    ]);
+
+    ffmpeg.on('error', (error) => {
+      console.error(`Error: ${error.message}`);
+    });
+
+    ffmpeg.on('close', () => {
+      console.log(existsSync(tempFile));
+      readFile(tempFile, (err, data) => {
+        if (err) {
+          console.error(`Error: ${err.message}`);
+        }
+
+        resolve(data);
+      });
+    });
+  });
+}
