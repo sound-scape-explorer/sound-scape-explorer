1a8b508c1f67085ab0c6c976cf6080bc7d22aec0
Bamdad Sabbagh <hi@bamdad.fr>
2024-03-11 16:17:21 +0100

    fix(App): Add new python build process + Purge old files

--- release.config.js
+++ release.config.js
@@ -1,57 +1,58 @@
 module.exports = {
-  branches: ['main', 'next/bamdad'],
+  branches: ["main", "next/bamdad"],
   plugins: [
     [
-      '@semantic-release/exec',
+      "@semantic-release/exec",
       {
-        'verifyReleaseCmd': 'echo ${nextRelease.version} > .VERSION',
+        verifyReleaseCmd: "echo ${nextRelease.version} > .VERSION",
       },
     ],
-    ['@semantic-release/commit-analyzer'],
-    '@semantic-release/release-notes-generator',
+    ["@semantic-release/commit-analyzer"],
+    "@semantic-release/release-notes-generator",
     [
-      '@semantic-release/changelog',
+      "@semantic-release/changelog",
       {
-        changelogFile: 'CHANGELOG.md',
+        changelogFile: "CHANGELOG.md",
       },
     ],
     [
-      '@semantic-release/npm',
+      "@semantic-release/npm",
       {
         npmPublish: false,
       },
     ],
+    ["semantic-release-pypi"],
     [
-      '@semantic-release/github',
+      "@semantic-release/github",
       {
         assets: [
-          'visualisation-binaries/*.deb',
-          'visualisation-binaries/*.dmg',
-          'visualisation-binaries/*.exe',
+          "visualisation-binaries/*.deb",
+          "visualisation-binaries/*.dmg",
+          "visualisation-binaries/*.exe",
         ],
       },
     ],
     [
-      '@qiwi/semantic-release-gh-pages-plugin',
+      "@qiwi/semantic-release-gh-pages-plugin",
       {
-        src: 'front/dist',
-        msg: 'Front: Published <%= nextRelease.gitTag %>',
+        src: "front/dist",
+        msg: "Front: Published <%= nextRelease.gitTag %>",
       },
     ],
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
         assets: [
-          'CHANGELOG.md',
-          'package.json',
-          'processing/setup.py',
-          'front/src/version.ts',
-          'audio/src/version.ts',
-          'visualisation/src/version.ts',
+          "CHANGELOG.md",
+          "package.json",
+          "processing/setup.py",
+          "front/src/version.ts",
+          "audio/src/version.ts",
+          "visualisation/src/version.ts",
         ],
         // eslint-disable-next-line no-template-curly-in-string
         message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
