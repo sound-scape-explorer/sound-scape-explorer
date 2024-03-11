8d58b3b6b50949b22b823803be45002c44f7d9bc
Bamdad Sabbagh <hi@bamdad.fr>
2024-03-11 14:07:10 +0100

    feat(Processing): Migrate to pyproject.toml

--- processing/processing/cli.py
+++ processing/processing/cli.py
@@ -1,15 +1,12 @@
 import argparse
 import os
 import platform
-import subprocess
 import sys
-from distutils.spawn import find_executable
 
 from processing.main import main
 from processing.utils.append_to_config import append_to_config
 from processing.utils.extract_config_from_storage import extract_config_from_storage
 from processing.utils.read_audio_path_from_config import read_audio_path_from_config
-from processing.utils.trim_quotes_if_needed import trim_quotes_if_needed
 from processing.utils.walk_directory import walk_directory
 
 
@@ -40,42 +37,13 @@ def start_processing():
     main(config_path=config_path)
 
 
-def start_visualisation():
-    update_python_path()
-    config_path = parse_arguments()
-
-    # parse config
-    audio_path = read_audio_path_from_config(config_path)
-
-    # navigate to app root directory
-    venv_path = sys.exec_prefix
-    os.chdir(venv_path)
-    os.chdir("..")
-
-    # get paths
-    ffmpeg_path = find_executable("ffmpeg")
-    ffprobe_path = find_executable("ffprobe")
-    audio_path = trim_quotes_if_needed(audio_path)
-
-    # create commands
-    audio_command = ["pnpm", "audio", ffmpeg_path, ffprobe_path, audio_path]
-    front_command = ["pnpm", "front"]
-
-    # spawn
-    audio_process = subprocess.Popen(audio_command)
-    front_process = subprocess.Popen(front_command)
-
-    audio_process.wait()
-    front_process.wait()
-
-
 def extract_config():
     update_python_path()
     storage_path = parse_arguments()
     extract_config_from_storage(storage_path)
 
 
-def start_fill():
+def fill_config():
     update_python_path()
     config_path = parse_arguments()
     audio_path = read_audio_path_from_config(config_path)
@@ -83,24 +51,16 @@ def start_fill():
     append_to_config(config_path, paths)
 
 
-def start_audio():
-    update_python_path()
-    config_path = parse_arguments()
+def download():
+    """Download dictionaries for offline use"""
 
-    # parse config
-    audio_path = read_audio_path_from_config(config_path)
+    from rich import print
+    from torch import hub
 
-    # navigate to app root directory
-    venv_path = sys.exec_prefix
-    os.chdir(venv_path)
-    os.chdir("..")
+    hub.load_state_dict_from_url(
+        "https://github.com/harritaylor/torchvggish/"
+        "releases/download/v0.1/vggish-10086976.pth",
+        progress=True,
+    )
 
-    # spawn audio and front modules
-    if platform.system() == "Windows":
-        audio_path = trim_quotes_if_needed(audio_path)
-        audio_command = ["pnpm", "audio", audio_path]
-        audio_process = subprocess.Popen(audio_command)
-        audio_process.wait()
-    else:
-        command = ["pnpm", "audio", audio_path]
-        subprocess.run(command)
+    print("You are now ready for offline use!")
