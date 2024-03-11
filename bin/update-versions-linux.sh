a1e384f3802b4b4043fdfae008ebf91110b89580
Bamdad Sabbagh <hi@bamdad.fr>
2024-03-11 17:20:01 +0100

    fix(Release): Avoid appending comma when replacing pyproject version to next

--- bin/update-versions-linux.sh
+++ bin/update-versions-linux.sh
@@ -3,4 +3,4 @@
 sed -i "s/VERSION = \(.\)*/VERSION = '$NEXT_VERSION';/" front/src/version.ts
 sed -i "s/VERSION = \(.\)*/VERSION = '$NEXT_VERSION';/" audio/src/version.ts
 sed -i "s/VERSION = \(.\)*/VERSION = '$NEXT_VERSION';/" visualisation/src/version.ts
-sed -i 's/version = \(.\)*/version = \"'"$NEXT_VERSION"'\",/' processing/pyproject.toml
+sed -i 's/version = \(.\)*/version = \"'"$NEXT_VERSION"'\"/' processing/pyproject.toml
