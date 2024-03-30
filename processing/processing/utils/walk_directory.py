0e3c8828292e9e24394d22de34636eb568f7a262
Bamdad Sabbagh <hi@bamdad.fr>
2024-03-30 18:43:30 +0100

    fix(Processing/AutoFill): Transform filename to be case insensitive

--- processing/processing/utils/walk_directory.py
+++ processing/processing/utils/walk_directory.py
@@ -8,15 +8,19 @@ def walk_directory(path: str) -> List[str]:
     supported_formats = [".wav", ".mp3", ".flac"]
 
     for dir_path, _, filenames in os.walk(path):
-        for f in filenames:
+        for filename in filenames:
             for format_ in supported_formats:
-                if f.endswith(format_):
-                    absolute_path = os.path.join(dir_path, f)
-                    relative_path = absolute_path.replace(path, "")
+                filename_lowercase = filename.lower()
 
-                    if platform.system() == "Windows":
-                        relative_path = relative_path.replace("\\", "/")
+                if not filename_lowercase.endswith(format_):
+                    continue
 
-                    relative_paths.append(relative_path)
+                absolute_path = os.path.join(dir_path, filename)
+                relative_path = absolute_path.replace(path, "")
+
+                if platform.system() == "Windows":
+                    relative_path = relative_path.replace("\\", "/")
+
+                relative_paths.append(relative_path)
 
     return relative_paths
