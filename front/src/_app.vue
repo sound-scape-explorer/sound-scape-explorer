59529d2d3e792408755f04391c0125eba89ef65b
Bamdad Sabbagh <hi@bamdad.fr>
2024-04-26 14:44:43 +0200

    refactor(Front): Merge explore SPA into app component

--- front/src/_app.vue
+++ front/src/_app.vue
@@ -7,9 +7,32 @@ import {
   NNotificationProvider,
   NSpace,
 } from 'naive-ui';
-import Explore from 'src/_explore.vue';
+import AppLoader from 'src/app/app-loader.vue';
 import AppLoading from 'src/app/app-loading.vue';
+import AppMenu from 'src/app/menu/app-menu.vue';
 import AppNotification from 'src/app/notification/app-notification.vue';
+import {useDraggables} from 'src/composables/draggables';
+import {useStorageReady} from 'src/composables/storage-ready';
+import {useWorker} from 'src/composables/worker';
+import Draggables from 'src/draggables/draggables.vue';
+import Scatter from 'src/scatter/scatter.vue';
+import {onMounted} from 'vue';
+
+// TODO: is the Suspense component actually needed?
+
+useWorker();
+const {isReady} = useStorageReady();
+const {store} = useDraggables();
+
+const showImport = () => {
+  if (isReady.value) {
+    return;
+  }
+
+  store.import = true;
+};
+
+onMounted(showImport);
 </script>
 
 <template>
@@ -31,7 +54,12 @@ import AppNotification from 'src/app/notification/app-notification.vue';
         </NNotificationProvider>
 
         <Suspense>
-          <Explore />
+          <div>
+            <AppMenu />
+            <AppLoader v-if="isReady" />
+            <Scatter v-if="isReady" />
+            <Draggables />
+          </div>
         </Suspense>
       </NLayout>
     </NLayout>
