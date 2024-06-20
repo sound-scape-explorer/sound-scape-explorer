96e2fc8ddcc060936f4049b726f645b61a56519e
Bamdad Sabbagh <hi@bamdad.fr>
2024-06-20 13:41:36 +0200

    fix(Front/Settings): Improve vue components

---
+++ front/src/draggables/settings/draggable-settings-item.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import {NTag} from 'naive-ui';
+
+interface Props {
+  title: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="draggable-settings-item">
+    <NTag
+      :bordered="false"
+      size="small"
+    >
+      {{ props.title }}
+    </NTag>
+    <span />
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.draggable-settings-item {
+  display: grid;
+  grid-template-columns: auto 1fr 10em;
+  gap: 0.5em;
+  width: 100%;
+
+  margin-bottom: 0.5em;
+}
+</style>
