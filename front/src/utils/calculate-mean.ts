aad702d9de95637a7357cda18094a5e44ad25f11
Bamdad Sabbagh <hi@bamdad.fr>
2024-06-30 18:40:29 +0200

    fix(Front/CalculateMean): Improve iteration

--- front/src/utils/calculate-mean.ts
+++ front/src/utils/calculate-mean.ts
@@ -1,9 +1,9 @@
-export function calculateMean(arr: number[]): number {
+export function calculateMean(array: number[]): number {
   let sum = 0;
 
-  for (let i = 0; i < arr.length; i += 1) {
-    sum += arr[i];
+  for (const value of array) {
+    sum += value;
   }
 
-  return sum / arr.length;
+  return sum / array.length;
 }
