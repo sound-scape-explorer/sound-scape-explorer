44569a8336f153d2b3e6147ba3350765d920f3e9
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-01 16:07:27 +0200

    feat(Front/DraggableMenu): Add size prop

--- front/src/app/draggable-menu/app-draggable-menu.vue
+++ front/src/app/draggable-menu/app-draggable-menu.vue
@@ -1,14 +1,47 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {computed} from 'vue';
+
+interface Props {
+  size?: 'small' | 'medium' | 'large';
+}
+
+const props = withDefaults(defineProps<Props>(), {size: 'small'});
+
+const classNames = computed<string>(() => {
+  let string = 'app-draggable-menu__form';
+
+  switch (props.size) {
+    case 'small': {
+      string += ' app-draggable-menu__form_s';
+      break;
+    }
+    case 'medium': {
+      string += ' app-draggable-menu__form_m';
+      break;
+    }
+    case 'large': {
+      string += ' app-draggable-menu__form_l';
+      break;
+    }
+  }
+
+  return string;
+});
+</script>
 
 <template>
   <div class="app-draggable-menu__container">
-    <div class="app-draggable-menu__form">
+    <div :class="classNames">
       <slot />
     </div>
   </div>
 </template>
 
 <style lang="scss">
+$s: 3rem;
+$m: 5rem;
+$l: 8rem;
+
 .app-draggable-menu__container {
   display: flex;
   justify-content: center;
@@ -21,9 +54,20 @@
 
 .app-draggable-menu__form {
   display: grid;
-  grid-template-columns: 3rem 1fr;
   gap: 0.5rem;
   width: 100%;
+}
+
+.app-draggable-menu__form_s {
+  grid-template-columns: $s 1fr;
+}
+
+.app-draggable-menu__form_m {
+  grid-template-columns: $m 1fr;
+}
+
+.app-draggable-menu__form_l {
+  grid-template-columns: $l 1fr;
 }
 
 .app-draggable-menu__row {
