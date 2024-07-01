45ae5f96fd527ca989b2c930c6c77a9224600caa
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-01 23:47:20 +0200

    feat(Front/Scatter): Use spectral as default color flavor

--- front/src/components/scatter/use-color-selection.ts
+++ front/src/components/scatter/use-color-selection.ts
@@ -3,7 +3,7 @@ import type {ColorType} from 'src/draggables/colors/use-color-type';
 import {ref} from 'vue';
 
 const type = ref<ColorType>('cycleDay');
-const flavor = ref<ColorFlavor>('Dark2');
+const flavor = ref<ColorFlavor>('Spectral');
 
 export function useColorSelection() {
   return {
