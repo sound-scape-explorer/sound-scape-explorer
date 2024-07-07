5612957774a2a132d6f4d641e11a225d07956094
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-07 20:38:01 +0200

    fix(Visualisation): Improve instanciations

--- visualisation/src/renderer/renderer.ts
+++ visualisation/src/renderer/renderer.ts
@@ -5,7 +5,8 @@ import {LoadedZone} from './controllers/LoadedZone';
 import {LoadingZone} from './controllers/LoadingZone';
 import {Title} from './controllers/Title';
 
-new Title();
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+const title = new Title();
 const loadingZone = new LoadingZone();
 const loadedZone = new LoadedZone();
 
