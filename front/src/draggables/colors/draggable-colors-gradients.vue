51870cbbb875ea117ad69131775c11e7f878c5ef
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-09 05:15:32 +0200

    pref(Front/Colors): Remove dead code

--- front/src/draggables/colors/draggable-colors-gradients.vue
+++ front/src/draggables/colors/draggable-colors-gradients.vue
@@ -1,18 +1,10 @@
 <script lang="ts" setup>
 import AppGradient from 'src/app/app-gradient.vue';
 import {useColorSelection} from 'src/components/scatter/use-color-selection';
-import {useScatterColorScale} from 'src/components/scatter/use-scatter-color-scale';
-import {useClientSettings} from 'src/composables/use-client-settings';
-import {useColorUser} from 'src/composables/use-color-user';
 import {useColorGradients} from 'src/draggables/colors/use-color-gradients';
 import {computed} from 'vue';
 
-const {scale, cyclingScale} = useScatterColorScale();
-const {scale: userScale} = useColorUser();
 const {criteria} = useColorSelection();
-const {isColorMapSwapped} = useClientSettings();
-
-const size = 100;
 
 const {cycleDayLabels, cycleDayColors, userColors, dayColors} =
   useColorGradients();
