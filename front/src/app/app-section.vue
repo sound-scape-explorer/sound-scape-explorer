f0c638b6c8274255c341129d508f3e55f8b055a7
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-10 00:46:50 +0200

    feat(Front/App): Add primitive section component

---
+++ front/src/app/app-section.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup="">
+interface Props {
+  height?: number; // percents
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  height: 20,
+});
+</script>
+
+<template>
+  <div
+    :style="{maxHeight: props.height + 'em'}"
+    class="container"
+  >
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  overflow-y: scroll;
+  padding-right: 10px;
+}
+</style>
