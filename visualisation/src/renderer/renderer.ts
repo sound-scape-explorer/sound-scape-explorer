89b03b1d4ebcfcb7ae020d3a434ddbd502cba515
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-09 18:50:31 +0200

    fix(Visualisation): Remove storage import for audio service start

--- visualisation/src/renderer/renderer.ts
+++ visualisation/src/renderer/renderer.ts
@@ -5,8 +5,7 @@ import {LoadedZone} from './controllers/LoadedZone';
 import {LoadingZone} from './controllers/LoadingZone';
 import {Title} from './controllers/Title';
 
-// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-const title = new Title();
+new Title();
 const loadingZone = new LoadingZone();
 const loadedZone = new LoadedZone();
 
