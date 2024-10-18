0a3dcde94c7c40f3e8ffa2f52e663df37446b215
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-18 05:30:38 +0200

    refactor(Front/Calendar): Split sliders and interests

--- front/src/draggables/calendar/draggable-calendar-slider.vue
+++ front/src/draggables/calendar/draggable-calendar-slider.vue
@@ -1,158 +1,20 @@
 <script lang="ts" setup="">
 import {SearchOutline} from '@vicons/ionicons5';
-import dayjs from 'dayjs';
 import {NSlider} from 'naive-ui';
 import AppButton from 'src/app/app-button.vue';
 import {useScatterFilterTime} from 'src/components/scatter/use-scatter-filter-time';
 import {useScatterLoading} from 'src/components/scatter/use-scatter-loading';
-import {useRanges} from 'src/composables/use-ranges';
-import {useReducerSelection} from 'src/composables/use-reducer-selection';
-import {useStorageAggregatedTimestamps} from 'src/composables/use-storage-aggregated-timestamps';
-import {SLIDER_LIMITS} from 'src/constants';
+import {useCalendarInterests} from 'src/draggables/calendar/use-calendar-interests';
+import {useCalendarSliders} from 'src/draggables/calendar/use-calendar-sliders';
 import {useDraggableCalendar} from 'src/draggables/calendar/use-draggable-calendar';
-import {mapRange} from 'src/utils/map-range';
-import {computed, ref, watch} from 'vue';
+import {watch} from 'vue';
 
-// todo: refactor me
-
-const {ranges} = useRanges();
-const {reducer} = useReducerSelection();
-const {aggregatedTimestamps} = useStorageAggregatedTimestamps();
 const {isLoading} = useScatterLoading();
-const {isActive, duration, current, min, max} = useDraggableCalendar();
-
-const zoomedSliderRef = ref<Slider | null>(null);
-const cachedSlidersRef = ref<Slider[]>();
-
-const sliders = computed<Slider[]>(() => {
-  if (ranges.value === null || reducer.value === null) {
-    return [];
-  }
-
-  const zoomedSlider = zoomedSliderRef.value;
-  const cachedSliders = cachedSlidersRef.value;
-
-  if (zoomedSlider !== null) {
-    return [zoomedSlider];
-  }
-
-  if (typeof cachedSliders !== 'undefined') {
-    return cachedSliders;
-  }
-
-  const sliders: Slider[] = [];
-
-  for (const range of ranges.value) {
-    const timeStart = dayjs(range.start).unix();
-    const timeEnd = dayjs(range.end).unix();
-    const timeBetween = Math.floor(timeStart + 0.5 * (timeEnd - timeStart));
-
-    if (min.value === -1 || timeStart < min.value) {
-      min.value = timeStart;
-    }
-
-    if (max.value === -1 || timeEnd > max.value) {
-      max.value = timeEnd;
-    }
-
-    const slider = {
-      key: range.index,
-      name: range.name,
-      min: timeStart,
-      max: timeEnd,
-      marks: {
-        [timeStart]: SLIDER_LIMITS.start,
-        [timeBetween]: range.name.toString(),
-        [timeEnd]: SLIDER_LIMITS.end,
-      },
-    } satisfies Slider;
-
-    sliders.push(slider);
-  }
-
-  // Can be improved to avoid collisions
-  sliders.sort((a, b) => a.min - b.max);
-
-  current.value = min.value;
-
-  cachedSlidersRef.value = sliders;
-
-  return sliders;
-});
-
-interface Interest {
-  key: number;
-  values: boolean[];
-}
-
-const interests = computed<Interest[]>(() => {
-  if (aggregatedTimestamps.value === null) {
-    return [];
-  }
-
-  const allTimestamps = aggregatedTimestamps.value.map((t) => t / 1000);
-
-  const interests: Interest[] = [];
-  const ignoreDecimalsFactor = 1 / duration.value;
-  const ignoreDecimals = (value: number) =>
-    Math.floor(ignoreDecimalsFactor * value);
-
-  for (const slider of sliders.value) {
-    const {min, max, key} = slider;
-    const timestamps = allTimestamps.filter(
-      (timestamp) => timestamp >= min && timestamp <= max,
-    );
-
-    const values = [];
-
-    for (let i = 0; i < 100; i += 1) {
-      const index = mapRange(i, 0, 100, min, max);
-
-      const trimmedTimestamps = timestamps.map((t) => ignoreDecimals(t));
-      const isInterest = trimmedTimestamps.includes(ignoreDecimals(index));
-
-      values.push(isInterest);
-    }
-
-    const interest: Interest = {
-      key: key,
-      values: values,
-    };
-
-    interests.push(interest);
-  }
-
-  return interests;
-});
-
-/**
- * Handlers
- */
-
-function resetZoom() {
-  zoomedSliderRef.value = null;
-}
-
-function toggleZoom(slider: Slider): void {
-  if (zoomedSliderRef.value !== null) {
-    resetZoom();
-    return;
-  }
-
-  zoomedSliderRef.value = slider;
-}
-
-interface Slider {
-  key: number;
-  name: string;
-  min: number;
-  max: number;
-  marks: {
-    [time: number]: string;
-  };
-}
-
+const {isActive, duration, current} = useDraggableCalendar();
 const {filterByTime} = useScatterFilterTime();
+const {sliders, toggleZoom} = useCalendarSliders();
+const {interests} = useCalendarInterests();
+
 watch([isActive, duration, current], filterByTime);
 </script>
 
