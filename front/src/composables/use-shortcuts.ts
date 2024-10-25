a6f5644133c40d4f603cc251d982a00dccccc9df
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-25 16:58:56 +0200

    fix(Front/Shortcuts): Sort alphabetically and remap view and calendar bindings

--- front/src/composables/use-shortcuts.ts
+++ front/src/composables/use-shortcuts.ts
@@ -8,7 +8,7 @@ export enum Shortcuts {
   open = 'o',
   settings = ',',
   help = '?',
-  view = 'v',
+  view = 'w',
   colors = 'c',
   labels = 'l',
   audio = 'a',
@@ -18,7 +18,7 @@ export enum Shortcuts {
   temporal = 't',
   histograms = 'j',
   heatmaps = 'h',
-  calendar = 'C',
+  calendar = 'v',
   timeline = 'T',
   selection = 's',
   selectHotkey = 'J',
@@ -31,16 +31,16 @@ interface ShortcutSerialized {
 }
 
 export function useKeyboardShortcuts() {
-  const shortcuts: ShortcutSerialized[] = Object.entries(Shortcuts).map(
-    (entry) => {
+  const shortcuts: ShortcutSerialized[] = Object.entries(Shortcuts)
+    .map((entry) => {
       const [name, keycode] = entry;
 
       return {
         keycode: keycode,
         name: name,
       };
-    },
-  );
+    })
+    .sort((a, b) => a.name.localeCompare(b.name));
 
   const getKey = (key: DraggableKey) => Shortcuts[key];
 
