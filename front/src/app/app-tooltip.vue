37999f94390982a0e54830a1f40a69d60f9f60f7
Bamdad Sabbagh <hi@bamdad.fr>
2024-11-01 15:07:27 +0100

    feat(Front/Tooltips): Add delay to hover effect

--- front/src/app/app-tooltip.vue
+++ front/src/app/app-tooltip.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup="">
 import {NTooltip} from 'naive-ui';
+import {TIMEOUT} from 'src/constants';
+import {ref} from 'vue';
 // @ts-expect-error underlying dependency of naive-ui
 // noinspection TypeScriptCheckImport
 import {type Placement} from 'vueuc/lib/binder/src/interface';
@@ -8,17 +10,47 @@ interface Props {
   placement?: Placement;
 }
 
+let t: number | null = null;
+
 const props = withDefaults(defineProps<Props>(), {placement: 'left'});
+
+const show = ref<boolean>(false);
+
+const handleMouseEnter = () => {
+  clear();
+
+  t = setTimeout(() => {
+    show.value = true;
+  }, TIMEOUT * 2);
+};
+
+const handleMouseLeave = () => {
+  clear();
+  show.value = false;
+};
+
+const clear = () => {
+  if (t) {
+    clearTimeout(t);
+    t = null;
+  }
+};
 </script>
 
 <template>
   <NTooltip
     :placement="props.placement"
-    trigger="hover"
+    :show="show"
+    trigger="manual"
   >
     <!--suppress VueUnrecognizedSlot -->
     <template #trigger>
-      <slot name="body" />
+      <div
+        @mouseenter="handleMouseEnter"
+        @mouseleave="handleMouseLeave"
+      >
+        <slot name="body" />
+      </div>
     </template>
 
     <slot name="tooltip" />
