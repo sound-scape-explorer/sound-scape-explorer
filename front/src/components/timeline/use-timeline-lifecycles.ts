db864682b21344956c5956d1852426384c3ee27e
Bamdad Sabbagh <hi@bamdad.fr>
2024-11-17 16:41:37 +0100

    refactor(Front/Timeline): Use ref for name options instead of multiple instances of computed

--- front/src/components/timeline/use-timeline-lifecycles.ts
+++ front/src/components/timeline/use-timeline-lifecycles.ts
@@ -1,12 +1,16 @@
 import {useTimelineHandlers} from 'src/components/timeline/use-timeline-handlers';
 import {useTimelineOptions} from 'src/components/timeline/use-timeline-options';
+import {useStorageRanges} from 'src/composables/use-storage-ranges';
 import {onMounted, watch} from 'vue';
 
 export function useTimelineLifecycles() {
-  const {names, name} = useTimelineOptions();
+  const {ranges} = useStorageRanges();
+  const {names, name, updateNames} = useTimelineOptions();
   const {updateName, updateRange} = useTimelineHandlers();
 
   onMounted(updateRange);
-  watch(name, updateRange);
+
+  watch(ranges, updateNames);
   watch(names, updateName);
+  watch(name, updateRange);
 }
--- front/src/components/timeline/use-timeline-options.ts
+++ front/src/components/timeline/use-timeline-options.ts
@@ -1,18 +1,24 @@
 import {useStorageRanges} from 'src/composables/use-storage-ranges';
 import {generateUniqueRangeSlug} from 'src/utils/config';
-import {computed, ref} from 'vue';
+import {ref} from 'vue';
 
 const name = ref<string>();
+const names = ref<string[]>([]);
 
 export function useTimelineOptions() {
   const {ranges} = useStorageRanges();
 
-  const names = computed(
-    () => ranges.value?.map((r) => generateUniqueRangeSlug(r)) ?? [],
-  );
+  const updateNames = () => {
+    if (ranges.value === null) {
+      return;
+    }
+
+    names.value = ranges.value.map((r) => generateUniqueRangeSlug(r));
+  };
 
   return {
     name: name,
     names: names,
+    updateNames: updateNames,
   };
 }
