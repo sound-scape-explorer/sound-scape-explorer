1e5b7774766cd0c8e904581efdfa97592873a537
Bamdad Sabbagh <hi@bamdad.fr>
2024-11-17 16:55:59 +0100

    perf(Front/Colors): Convert computeds to refs for better compasability

--- front/src/draggables/colors/use-color-selection.ts
+++ front/src/draggables/colors/use-color-selection.ts
@@ -4,7 +4,7 @@ import {SettingKey} from 'src/common/setting-key';
 import {useIndicators} from 'src/composables/use-indicators';
 import {useStorageLabels} from 'src/composables/use-storage-labels';
 import {type ColorFlavor} from 'src/constants';
-import {computed, ref} from 'vue';
+import {ref} from 'vue';
 
 type ColorCategory = 'Default' | 'Labels' | 'Indicators';
 type ColorCriteria =
@@ -19,8 +19,20 @@ const flavor = useStorage<ColorFlavor>(
   settingDefaults.colorsFlavor,
 );
 
+const defaultCriterias: ColorCriteria[] = [
+  'cycleDay',
+  'isDay',
+  'intervalIndex',
+  'by1h',
+  'by10min',
+];
+
+const labelCriterias = ref<ColorCriteria[]>([]);
+const indicatorCriterias = ref<ColorCriteria[]>([]);
 const criteria = ref<ColorCriteria>('cycleDay');
 const category = ref<ColorCategory>('Default');
+const criterias = ref<ColorCriteria[]>(defaultCriterias);
+const criteriaIndex = ref<number>(-1);
 
 const categories = ref<ColorCategory[]>(['Default', 'Labels', 'Indicators']);
 
@@ -28,44 +40,45 @@ export function useColorSelection() {
   const {labelProperties} = useStorageLabels();
   const {names} = useIndicators();
 
-  // todo: maybe extract me?
-  const criteriaIndex = computed(() => criterias.value.indexOf(criteria.value));
+  const updateCriterias = () => {
+    switch (category.value) {
+      case 'Default':
+        criterias.value = defaultCriterias;
+        break;
+
+      case 'Labels':
+        criterias.value = labelCriterias.value;
+        break;
+
+      case 'Indicators':
+        criterias.value = indicatorCriterias.value;
+        break;
+    }
+  };
+
+  const updateCriteriaIndex = () => {
+    criteriaIndex.value = criterias.value.indexOf(criteria.value);
+  };
 
   const reset = () => {
     flavor.value = settingDefaults.colorsFlavor;
   };
 
-  const defaultCriterias = computed(
-    () =>
-      [
-        'cycleDay',
-        'isDay',
-        'intervalIndex',
-        'by1h',
-        'by10min',
-      ] as ColorCriteria[],
-  );
-
-  const labelCriterias = computed(
-    () => (labelProperties.value as ColorCriteria[]) ?? [],
-  );
-  const indicatorCriterias = computed(
-    () => (names.value as ColorCriteria[]) ?? [],
-  );
-
-  const criterias = computed<ColorCriteria[]>(() => {
-    switch (category.value) {
-      case 'Default': {
-        return defaultCriterias.value;
-      }
-      case 'Labels': {
-        return labelCriterias.value;
-      }
-      case 'Indicators': {
-        return indicatorCriterias.value;
-      }
+  const updateLabelCriterias = () => {
+    if (labelProperties.value === null) {
+      return;
     }
-  });
+
+    labelCriterias.value = labelProperties.value as ColorCriteria[];
+  };
+
+  const updateIndicatorCriterias = () => {
+    if (names.value === null) {
+      return;
+    }
+
+    indicatorCriterias.value = names.value as ColorCriteria[];
+  };
 
   const handleLabelClick = (property: string) => {
     if (category.value !== 'Labels') {
@@ -82,9 +95,13 @@ export function useColorSelection() {
     flavor: flavor,
     criteria: criteria,
     criterias: criterias,
+    updateCriterias: updateCriterias,
     criteriaIndex: criteriaIndex,
+    updateCriteriaIndex: updateCriteriaIndex,
     category: category,
     categories: categories,
     handleLabelClick: handleLabelClick,
+    updateLabelCriterias: updateLabelCriterias,
+    updateIndicatorCriterias: updateIndicatorCriterias,
   };
 }
