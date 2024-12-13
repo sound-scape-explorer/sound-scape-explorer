83f481424ba3b5ed03d6bd990d0349c2ed7e0aaa
Bamdad Sabbagh <hi@bamdad.fr>
2024-12-13 09:29:07 +0100

    fix(Visualisation): Add path alias resolution

--- visualisation/vite.renderer.config.ts
+++ visualisation/vite.renderer.config.ts
@@ -1,4 +1,11 @@
 import {defineConfig} from 'vite';
+import {resolve} from 'path';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    alias: {
+      src: resolve(__dirname, './src'),
+    },
+  },
+});
