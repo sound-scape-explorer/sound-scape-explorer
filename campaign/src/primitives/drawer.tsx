3c8a106991481247ffd803a9747861d10f4e0f61
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-18 19:15:52 +0100

    fix(Campaign): Add dark mode to drawers

--- campaign/src/primitives/drawer.tsx
+++ campaign/src/primitives/drawer.tsx
@@ -1,7 +1,9 @@
 import {Button, Drawer as BDrawer} from '@blueprintjs/core';
 import {Cross} from '@blueprintjs/icons';
+import clsx from 'clsx';
 import {JSX, useState} from 'react';
 import {ICON_SIZE} from 'src/constants.ts';
+import {useTheme} from 'src/hooks/use-theme.ts';
 
 interface Props {
   content: JSX.Element;
@@ -12,11 +14,13 @@ export function Drawer({content, children}: Props) {
   const [isOpen, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const {isDark} = useTheme();
 
   return (
     <>
       <div onClick={handleOpen}>{children}</div>
       <BDrawer
+        className={clsx(isDark && 'bp5-dark')}
         isOpen={isOpen}
         onClose={handleClose}
         canEscapeKeyClose
