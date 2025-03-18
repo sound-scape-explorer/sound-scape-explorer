c996a6b81627b237cbd7faddc90a2d6437d7cf20
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-18 18:53:13 +0100

    feat(Campaign/Settings): Add help for audio folder path

--- campaign/src/panels/settings/settings-panel-audio-path.tsx
+++ campaign/src/panels/settings/settings-panel-audio-path.tsx
@@ -1,7 +1,8 @@
-import {Tooltip} from '@blueprintjs/core';
+import {Callout, Tooltip} from '@blueprintjs/core';
 import clsx from 'clsx';
 import {useSettingsState} from 'src/hooks/use-settings-state.ts';
 import {useSettingsValidation} from 'src/hooks/use-settings-validation.ts';
+import {Drawer} from 'src/primitives/drawer.tsx';
 import {TextInput} from 'src/primitives/text-input.tsx';
 
 import styles from './settings-panel.module.scss';
@@ -15,7 +16,29 @@ export function SettingsPanelAudioPath() {
       className={clsx(styles.row, 'align gap')}
       onDrop={handleAudioDrop}
     >
-      <b className={styles.rowTitle}>Audio Path</b>
+      <Drawer
+        content={
+          <div className="flex column gap mt">
+            <Callout
+              compact
+              title="How to update?"
+            >
+              <div>Drag a folder over the input field to change its value.</div>
+            </Callout>
+            <Callout
+              compact
+              title="Validation status"
+            >
+              <div>
+                Status is valid when the full paths from audio folder and audio
+                paths exist.
+              </div>
+            </Callout>
+          </div>
+        }
+      >
+        <b className={clsx(styles.rowTitle, 'flex grow help')}>Audio Path</b>
+      </Drawer>
       <Tooltip content="The path to your audio folder">
         <TextInput
           value={settings.audioPath}
