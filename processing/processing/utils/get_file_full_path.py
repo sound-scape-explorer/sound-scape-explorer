68949034ae2f638637668c42ab8964a287460c90
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-26 00:29:09 +0100

    fix(Processing): Fix path joining issue #242

--- processing/processing/utils/get_file_full_path.py
+++ processing/processing/utils/get_file_full_path.py
@@ -1,11 +1,7 @@
-from processing.utils.print_action import print_action
+import os
 
 
 def get_file_full_path(filename: str, audio_folder: str):
-    if filename[0] == "/":
-        path = f"{audio_folder}{filename}"
-    else:
-        print_action(f"File name {filename} does not start with a /", "error")
-        path = f"{audio_folder}/{filename}"
-
-    return path
+    relative_path = filename[1:] if filename.startswith(os.path.sep) else filename
+    full_path = os.path.join(audio_folder, relative_path)
+    return full_path
