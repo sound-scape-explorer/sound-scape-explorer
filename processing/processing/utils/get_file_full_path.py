a1487828e2468f2258b8df112240773c85425e44
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-26 03:32:30 +0100

    fix(Processing): Add forward slash convention handling for filenames

--- processing/processing/utils/get_file_full_path.py
+++ processing/processing/utils/get_file_full_path.py
@@ -2,6 +2,11 @@ import os
 
 
 def get_file_full_path(filename: str, audio_folder: str):
-    relative_path = filename[1:] if filename.startswith(os.path.sep) else filename
+    relative_path = filename
+
+    # TODO: Will drop the '/' filename start convention later
+    if filename.startswith(os.path.sep) or filename.startswith("/"):
+        relative_path = filename[1:]
+
     full_path = os.path.join(audio_folder, relative_path)
     return full_path
