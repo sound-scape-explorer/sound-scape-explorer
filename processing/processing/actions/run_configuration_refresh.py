ed10b746791d97e4cde501bc2d9a75db03a59ca2
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-31 15:10:49 +0200

    feat(Processing): Add silent config refresh for other actions

--- processing/processing/actions/run_configuration_refresh.py
+++ processing/processing/actions/run_configuration_refresh.py
@@ -3,7 +3,11 @@ from processing.new.ConfigManager import ConfigManager
 from processing.printers.print_action import print_action
 
 
-def run_configuration_refresh(context: Context):
-    print_action("Configuration refresh started!", "start")
+def run_configuration_refresh(context: Context, is_silent: bool = False):
+    if not is_silent:
+        print_action("Configuration refresh started!", "start")
+
     ConfigManager.to_storage(context)
-    print_action("Configuration refresh completed!", "end")
+
+    if not is_silent:
+        print_action("Configuration refresh completed!", "end")
--- processing/processing/menu.py
+++ processing/processing/menu.py
@@ -12,11 +12,11 @@ from processing.actions.run_mdm_export import run_mdm_export
 from processing.actions.run_reductions import run_reductions
 from processing.actions.run_relative_trajectories import run_relative_trajectories
 from processing.actions.run_trajectories import run_trajectories
+from processing.askers.ask_menu import ask_menu
 from processing.common.MenuChoice import MenuChoice
 from processing.context import Context
 from processing.new.logger import configure_logger
 from processing.new.print_settings import print_settings
-from processing.askers.ask_menu import ask_menu
 from processing.utils.prettify_exceptions import prettify_exceptions
 from processing.utils.quit_application import quit_application
 
@@ -41,7 +41,7 @@ def menu(config_path: str, is_debug=False):
 
             action_map = {
                 MenuChoice.RunAll.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_extractions_aggregations(context),
                     run_reductions(context),
                     run_computations(context),
@@ -50,46 +50,46 @@ def menu(config_path: str, is_debug=False):
                     run_relative_trajectories(context),
                     run_digestions(context),
                 ),
-                MenuChoice.RunConfigurationRefresh.value: lambda: run_configuration_refresh(
-                    context
+                MenuChoice.RunConfigurationRefresh.value: lambda: (
+                    run_configuration_refresh(context),
                 ),
                 MenuChoice.RunExtractionsAggregations.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_extractions_aggregations(context),
                 ),
                 MenuChoice.RunReductions.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_reductions(context),
                 ),
                 MenuChoice.RunComputations.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_computations(context),
                 ),
                 MenuChoice.RunComputationsPurge.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_computations_purge(context),
                 ),
                 MenuChoice.RunAutoclusters.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_autoclusters(context),
                 ),
                 MenuChoice.RunTrajectories.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_trajectories(context),
                 ),
                 MenuChoice.RunRelativeTrajectories.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_relative_trajectories(context),
                 ),
                 MenuChoice.RunDigestions.value: lambda: (
-                    run_configuration_refresh(context),
+                    run_configuration_refresh(context, is_silent=True),
                     run_digestions(context),
                 ),
-                MenuChoice.RunDataframeExport.value: lambda: run_dataframe_export(
-                    context
+                MenuChoice.RunDataframeExport.value: lambda: (
+                    run_dataframe_export(context),
                 ),
-                MenuChoice.RunComputationsExport.value: lambda: run_computations_export(
-                    context
+                MenuChoice.RunComputationsExport.value: lambda: (
+                    run_computations_export(context),
                 ),
                 MenuChoice.RunMdmExport.value: lambda: run_mdm_export(context),
             }
