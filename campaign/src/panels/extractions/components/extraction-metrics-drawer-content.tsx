35dd4086979ba416917bb44c1b3df185f6096adb
Bamdad Sabbagh <hi@bamdad.fr>
2025-05-26 16:05:28 +0200

    feat(campaign): add link to scikit doc for metrics

--- campaign/src/panels/extractions/components/extraction-metrics-drawer-content.tsx
+++ campaign/src/panels/extractions/components/extraction-metrics-drawer-content.tsx
@@ -1,5 +1,6 @@
 import {MetricImpl} from '@shared/enums';
 import {DrawerContent} from 'src/primitives/drawer-content.tsx';
+import {Link} from 'src/primitives/link.tsx';
 
 export function ExtractionMetricsDrawerContent() {
   return (
@@ -9,9 +10,29 @@ export function ExtractionMetricsDrawerContent() {
         [MetricImpl.enum.MEAN_SPREADING, 'The mean spreading.'],
         [
           MetricImpl.enum.SILHOUETTE,
-          'The silhouette between subtypes of a given cluster.',
+          <div
+            className="flex column"
+            key={MetricImpl.enum.SILHOUETTE}
+          >
+            <span>The silhouette between subtypes of a given cluster.</span>
+            <Link href="https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html">
+              scikit-learn doc
+            </Link>
+          </div>,
         ],
-        [MetricImpl.enum.CONTINGENCY, 'Contingency between two clusters.'],
+
+        [
+          MetricImpl.enum.CONTINGENCY,
+          <div
+            className="flex column"
+            key={MetricImpl.enum.CONTINGENCY}
+          >
+            <span>Contingency between two clusters.</span>
+            <Link href="https://scikit-learn.org/stable/modules/generated/sklearn.metrics.cluster.contingency_matrix.html">
+              scikit-learn doc
+            </Link>
+          </div>,
+        ],
         [
           MetricImpl.enum.OVERLAP,
           'The overlap between subtypes of a given cluster.',
