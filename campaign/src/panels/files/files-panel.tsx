669ba5840f8f413b9fb586d3f47e3941c973add3
Bamdad Sabbagh <hi@bamdad.fr>
2025-07-16 07:23:05 +0200

    fix(campaign): add help for site

--- campaign/src/panels/files/files-panel.tsx
+++ campaign/src/panels/files/files-panel.tsx
@@ -2,11 +2,18 @@ import {Card} from '@blueprintjs/core';
 import {FilesCallout} from 'src/components/files-callout.tsx';
 import {FilesTable} from 'src/panels/files/components/files-table.tsx';
 import {FilesTagging} from 'src/panels/files/components/files-tagging.tsx';
+import {FilesHelpContent} from 'src/panels/files/files-help-content.tsx';
+import {HelpDrawer} from 'src/primitives/help-drawer.tsx';
 
 export function FilesPanel() {
   return (
     <Card className="flex column gap">
-      <FilesCallout />
+      <div className="flex gap center">
+        <FilesCallout />
+        <HelpDrawer>
+          <FilesHelpContent />
+        </HelpDrawer>
+      </div>
       <FilesTagging />
       <FilesTable />
     </Card>
