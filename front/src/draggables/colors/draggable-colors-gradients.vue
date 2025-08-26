de6a64f8a285df89e153be4ac0b6c0c9fc8071d9
Bamdad Sabbagh <hi@bamdad.fr>
2025-08-26 22:43:33 +0200

    feat(front): add color gradient hover tooltips for numeric tags

--- front/src/draggables/colors/draggable-colors-gradients.vue
+++ front/src/draggables/colors/draggable-colors-gradients.vue
@@ -3,14 +3,36 @@ import AppGradient from 'src/app/app-gradient.vue';
 import {useColorGradients} from 'src/draggables/colors/use-color-gradients';
 import {useColorSelection} from 'src/draggables/colors/use-color-selection';
 import {computed} from 'vue';
+import {useTagNumeric} from 'src/draggables/tags/use-tag-numeric';
+import {useColorByTag} from 'src/draggables/colors/use-color-by-tag';
 
 const {criteria} = useColorSelection();
+const {isEnabled: isNumeric} = useTagNumeric();
+const {min, max} = useColorByTag();
 
 const {cycleDayLabels, cycleDayColors, userColors, dayColors, cycleDayLegend} =
   useColorGradients();
 
-const isElse = computed(() => {
-  return criteria.value !== 'cycleDay' && criteria.value !== 'isDay';
+const isPreset = computed(() => {
+  return criteria.value === 'cycleDay' || criteria.value === 'isDay';
+});
+
+const userLabels = computed<string[]>(() => {
+  if (!isNumeric.value || min.value === null || max.value === null) {
+    return [];
+  }
+
+  const size = userColors.value.length;
+
+  const labels: string[] = [];
+
+  for (let i = 0; i < size; i += 1) {
+    const value = min.value + (i * (max.value - min.value)) / (size - 1);
+    const int = Math.round(value);
+    labels.push(int.toString());
+  }
+
+  return labels;
 });
 </script>
 
@@ -34,7 +56,8 @@ const isElse = computed(() => {
   />
 
   <AppGradient
-    v-if="isElse"
+    v-if="!isPreset"
     :colors="userColors"
+    :labels="userLabels"
   />
 </template>
