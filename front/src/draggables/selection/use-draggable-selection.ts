009994c9d46a60d3e3170b50b1849962f71f64da
Bamdad Sabbagh <hi@bamdad.fr>
2025-10-10 22:33:22 +0200

    fix(front): reset spatial selection angles on view unload

--- front/src/draggables/selection/use-draggable-selection.ts
+++ front/src/draggables/selection/use-draggable-selection.ts
@@ -1,3 +1,4 @@
+import {useSelectionState} from 'src/draggables/selection/use-selection-state';
 import {ref} from 'vue';
 
 const isActive = ref<boolean>(false); // render into display
@@ -5,6 +6,8 @@ const isFiltering = ref<boolean>(false); // filter actual data
 const isWireframe = ref<boolean>(true); // render as wireframe
 
 export function useDraggableSelection() {
+  const {resetAngles} = useSelectionState();
+
   const reset = () => {
     if (isActive.value) {
       isActive.value = false;
@@ -13,6 +16,8 @@ export function useDraggableSelection() {
     if (isFiltering.value) {
       isFiltering.value = false;
     }
+
+    resetAngles();
   };
 
   return {
