601d27ea0aae8d385c578faf12e064e054eaff3d
Bamdad Sabbagh <hi@bamdad.fr>
2025-10-11 19:50:46 +0200

    fix(front): improve shortcuts readability

--- front/src/composables/use-shortcuts.ts
+++ front/src/composables/use-shortcuts.ts
@@ -4,26 +4,26 @@ const draggablePrefix = '_draggable';
 
 export enum Shortcut {
   _draggableFocus = 'Esc',
-  _draggableCycleNext = 'Tab', // just for doc
-  _draggableCyclePrevious = 'Shift+Tab', // just for doc
+  _draggableCycleRecent = 'Tab', // just for doc
+  _draggableCycleAll = '⇧+Tab', // just for doc
   _draggableClose = 'x',
-  _draggableCloseExceptCurrent = 'X',
+  _draggableCloseExceptCurrent = '⇧ X',
   open = 'o',
   settings = ',',
   help = '?',
   view = 'w',
   colors = 'c',
+  calendar = 'v',
   tags = 't',
   selection = 's',
+  temporal = '⇧ T',
+  heatmaps = 'h',
+  histograms = 'j',
   audio = 'a',
+  audioPlayPause = ' ',
   details = 'd',
   trajectories = 'y',
-  relativeTrajectories = 'Y',
-  temporal = 'T',
-  histograms = 'j',
-  heatmaps = 'h',
-  calendar = 'v',
-  audioPlayPause = ' ',
+  relativeTrajectories = '⇧ Y',
 }
 
 export interface ShortcutSerialized {
@@ -55,7 +55,7 @@ Object.entries(Shortcut).forEach((entry) => {
   shortcuts.push(shortcut);
 });
 
-shortcuts.sort((a, b) => a.name.localeCompare(b.name));
+// shortcuts.sort((a, b) => a.name.localeCompare(b.name));
 
 export function useKeyboardShortcuts() {
   const getKey = (key: DraggableKey) => Shortcut[key];
