06ed0657d185a933c4364ce4450074b355b1cd57
Bamdad Sabbagh <hi@bamdad.fr>
2025-10-13 20:31:13 +0200

    feat(front): improve audio layout by cycling through first window details

--- front/src/draggables/audio/draggable-audio-menu-details.vue
+++ front/src/draggables/audio/draggable-audio-menu-details.vue
@@ -1,18 +1,63 @@
 <script lang="ts" setup="">
 import {useInterval} from 'src/composables/use-interval';
 import {useAudioFile} from 'src/draggables/audio/use-audio-file';
+import {ref} from 'vue';
+import {z} from 'zod';
 
 const {currentIndex} = useInterval();
 const {window} = useAudioFile();
+
+const Display = z.enum(['index', 'file', 'site']);
+// eslint-disable-next-line no-redeclare
+type Display = z.infer<typeof Display>;
+
+const display = ref<Display>(Display.enum.file);
+
+const cycle = () => {
+  const options = Display.options;
+  let next = options.indexOf(display.value) + 1;
+
+  if (next >= options.length) {
+    next = 0;
+  }
+
+  display.value = options[next];
+};
 </script>
 
 <template>
-  <span>Interval</span>
-  <span>{{ currentIndex }}</span>
+  <span
+    v-if="display === Display.enum.index"
+    :class="$style.hover"
+    @click="cycle"
+  >
+    Interval
+  </span>
+  <span v-if="display === Display.enum.index">{{ currentIndex }}</span>
 
-  <span>File</span>
-  <span>{{ window?.file.Path }}</span>
+  <span
+    v-if="display === Display.enum.file"
+    :class="$style.hover"
+    @click="cycle"
+  >
+    File
+  </span>
+  <span v-if="display === Display.enum.file">{{ window?.file.Path }}</span>
 
-  <span>Site</span>
-  <span>{{ window?.file.Site }}</span>
+  <span
+    v-if="display === Display.enum.site"
+    :class="$style.hover"
+    @click="cycle"
+  >
+    Site
+  </span>
+  <span v-if="display === Display.enum.site">{{ window?.file.Site }}</span>
 </template>
+
+<style lang="scss" module>
+.hover {
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
