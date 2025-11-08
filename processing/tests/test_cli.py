645591a2929dbc7c738761c5262445720ebd8e3b
Bamdad Sabbagh <hi@bamdad.fr>
2025-11-08 15:08:16 +0100

    test(processing): add cli memory cases

--- processing/tests/test_cli.py
+++ processing/tests/test_cli.py
@@ -6,23 +6,62 @@ from processing.common.MenuChoice import MenuChoice
 
 
 def test_cli_with_invalid_config():
-    from processing.cli import main
+    with (
+        patch("sys.argv", ["sse", "impossible.json"]),
+        pytest.raises(FileNotFoundError),
+    ):
+        from processing.cli import main
 
-    with patch("sys.argv", ["sse", "impossible.json"]):
-        with pytest.raises(FileNotFoundError):
-            main()
+        main()
 
 
 def test_cli_with_valid_config(config_path):
-    from processing.cli import main
-
     with (
         patch("sys.argv", ["sse", config_path]),
         patch("processing.menu.prompt_menu") as mock_prompt,
         pytest.raises(SystemExit) as sys_exit,
     ):
+        from processing.cli import main
+
         mock_prompt.return_value = MenuChoice.QUIT.value
         main()
         assert sys_exit.value.code == 0
 
 
+def test_cli_with_low_memory_one_gig(config_path):
+    import subprocess
+
+    result = subprocess.run(
+        ["sse", config_path, "-m", "1024"],
+        capture_output=True,
+        text=True,
+    )
+
+    assert result.returncode == 1
+    assert "Memory limit exceeded" in result.stdout
+
+
+def test_cli_with_low_memory_two_gigs(config_path):
+    import subprocess
+
+    result = subprocess.run(
+        ["sse", config_path, "-m", "2048"],
+        capture_output=True,
+        text=True,
+    )
+
+    assert result.returncode == 1
+    assert "Memory limit exceeded" in result.stdout
+
+
+def test_cli_with_low_memory_four_gigs(config_path):
+    with (
+        patch("sys.argv", ["sse", config_path, "-m", "4096"]),
+        patch("processing.menu.prompt_menu") as mock_prompt,
+        pytest.raises(SystemExit) as sys_exit,
+    ):
+        from processing.cli import main
+
+        mock_prompt.return_value = MenuChoice.QUIT.value
+        main()
+        assert sys_exit.value.code == 0
