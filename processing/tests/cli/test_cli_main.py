9e6f81facd48ac9419631eabf12810e8220fc1e4
Bamdad Sabbagh <hi@bamdad.fr>
2025-11-10 12:40:19 +0100

    test(processing): add CLI test case for CPU only mode

--- processing/tests/cli/test_cli_main.py
+++ processing/tests/cli/test_cli_main.py
@@ -28,6 +28,19 @@ def test_valid_config(config_path):
         assert sys_exit.value.code == 0
 
 
+def test_valid_config_cpu(config_path):
+    with (
+        patch("sys.argv", ["sse", config_path, "--cpu"]),
+        patch("processing.menu.prompt_menu") as mock_prompt,
+        pytest.raises(SystemExit) as sys_exit,
+    ):
+        from processing.cli import main
+
+        mock_prompt.return_value = MenuChoice.QUIT.value
+        main()
+        assert sys_exit.value.code == 0
+
+
 def test_low_memory_one_gig(config_path):
     import subprocess
 
@@ -52,21 +65,3 @@ def test_low_memory_two_gigs(config_path):
 
     assert result.returncode == 1
     assert "Memory limit exceeded" in result.stdout
-
-
-def test_memory_four_gigs(config_path):
-    """
-    This should display the menu even if
-    4Gb won't be enough to load some heavy models
-    like birdNET
-    """
-    with (
-        patch("sys.argv", ["sse", config_path, "-m", "4096"]),
-        patch("processing.menu.prompt_menu") as mock_prompt,
-        pytest.raises(SystemExit) as sys_exit,
-    ):
-        from processing.cli import main
-
-        mock_prompt.return_value = MenuChoice.QUIT.value
-        main()
-        assert sys_exit.value.code == 0
