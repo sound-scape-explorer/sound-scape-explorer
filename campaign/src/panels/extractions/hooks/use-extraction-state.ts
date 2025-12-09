5d45c2ad798e8e30483cbefa6218a694eaa5db71
Bamdad Sabbagh <hi@bamdad.fr>
2025-12-09 12:23:01 +0100

    chore(campaign): remove debug print

--- campaign/src/panels/extractions/hooks/use-extraction-state.ts
+++ campaign/src/panels/extractions/hooks/use-extraction-state.ts
@@ -81,8 +81,6 @@ export function useExtractionState() {
 
       const extraction = newExtractions.find((e) => e.index === 0);
 
-      console.log(newExtractions);
-
       if (!extraction) {
         return;
       }
