4382030b65fd5affba3d5d15508596b0d4bd4e0b
Bamdad Sabbagh <hi@bamdad.fr>
2025-12-28 20:59:18 +0100

    perf(front): remove dead code

--- front/src/composables/use-storage-aggregated-interval-details.ts
+++
@@ -1,8 +0,0 @@
-// INFO: A block corresponds to one audio
-// todo: replace me from intervals.windows
-export interface BlockDetails {
-  start: number; // ms
-  fileStart: number; // relative
-  file: string; // path
-  fileIndex: number;
-}
