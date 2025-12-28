9266219c917367a5cb8b6b632dcff19dbb3c7d98
Bamdad Sabbagh <hi@bamdad.fr>
2025-12-28 23:02:33 +0100

    fix(audio): throw better errors

--- audio/src/validate-query.ts
+++ audio/src/validate-query.ts
@@ -10,19 +10,19 @@ export function validateQuery(query: ParsedQs): Query {
   const file = query.file;
 
   if (typeof file !== 'string') {
-    throw new Error('File is not a string');
+    throw new TypeError('File is not a string');
   }
 
   const start = query.start;
 
   if (typeof start !== 'string') {
-    throw new Error('Start is not a string');
+    throw new TypeError('Start is not a string');
   }
 
   const end = query.end;
 
   if (typeof end !== 'string') {
-    throw new Error('End is not a string');
+    throw new TypeError('End is not a string');
   }
 
   return {
