0f03dc7805ef224f999d5a7381e2264c19e749b4
Bamdad Sabbagh <hi@bamdad.fr>
2025-12-28 22:58:49 +0100

    fix(audio): prevent sending detailed HTML on error

--- audio/src/main.ts
+++ audio/src/main.ts
@@ -104,8 +104,8 @@ app.get('/get', async (req, res) => {
     res.end(slice);
   } catch (error) {
     if (error instanceof Error) {
-      res.writeHead(500, {'Content-Type': 'text/html'});
-      res.end(error.message);
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Internal server error');
     }
   }
 });
