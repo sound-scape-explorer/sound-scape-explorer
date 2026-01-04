327dd140f2055021d991487dfab5a42183a0289a
Bamdad Sabbagh <hi@bamdad.fr>
2026-01-04 02:56:17 +0100

    fix(audio): add graceful shutdown and temp file purge

--- audio/src/constants.ts
+++ audio/src/constants.ts
@@ -1,1 +1,5 @@
+import os from 'node:os';
+import path from 'node:path';
+
 export const SCRATCH_FILENAME = 'sse-scratch';
+export const SCRATCH_PATH = path.join(os.tmpdir(), `${SCRATCH_FILENAME}.wav`);
--- audio/src/slice-audio.ts
+++ audio/src/slice-audio.ts
@@ -1,14 +1,9 @@
 import {spawn} from 'node:child_process';
 import {existsSync, unlinkSync} from 'node:fs';
-import os from 'node:os';
-import path from 'node:path';
 
-import {SCRATCH_FILENAME} from './constants';
+import {SCRATCH_PATH} from './constants';
 import {readFileAsync} from './read-file-async';
 import {validateFfmpeg} from './validate-ffmpeg';
-
-const tempPath = path.join(os.tmpdir(), `${SCRATCH_FILENAME}.wav`);
-console.log(`temp path: ${tempPath}`);
 
 export function sliceAudio(
   ffmpegPath: string,
@@ -17,8 +12,8 @@ export function sliceAudio(
   end: number,
 ): Promise<Buffer> {
   return new Promise((resolve) => {
-    if (existsSync(tempPath)) {
-      unlinkSync(tempPath);
+    if (existsSync(SCRATCH_PATH)) {
+      unlinkSync(SCRATCH_PATH);
     }
 
     const duration = end - start;
@@ -33,7 +28,7 @@ export function sliceAudio(
       start.toString(),
       '-t',
       duration.toString(),
-      tempPath,
+      SCRATCH_PATH,
     ]);
 
     ffmpeg.on('error', (error) => {
@@ -41,7 +36,7 @@ export function sliceAudio(
     });
 
     ffmpeg.on('close', async () => {
-      const data = await readFileAsync(tempPath);
+      const data = await readFileAsync(SCRATCH_PATH);
       resolve(data);
     });
   });
