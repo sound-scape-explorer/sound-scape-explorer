1aeb57384c00464851ce08396f28f9d6cfa71bc0
Bamdad Sabbagh <hi@bamdad.fr>
2026-01-08 18:46:21 +0100

    fix(visualisation): add link to download latest software version

--- visualisation/src/renderer/views/version-status-view.ts
+++ visualisation/src/renderer/views/version-status-view.ts
@@ -2,24 +2,39 @@ import {REFS} from 'src/renderer/constants';
 import {AbstractStatusView} from 'src/renderer/views/abstract-status-view';
 
 const node = REFS.VERSION_STATUS;
+const bulmaHoverableClass = 'is-hoverable';
 
 export class VersionStatusView extends AbstractStatusView {
+  private upstreamVersion: string | null;
+
   public constructor() {
     super(node);
+    this.upstreamVersion = null;
   }
 
   public showUpToDate() {
     this.renderSuccess();
+    node.classList.remove(bulmaHoverableClass);
+    node.removeEventListener('click', this.handleClick.bind(this));
     node.textContent = 'Up to date';
   }
 
   public showOutdated(upstreamVersion: string) {
+    this.upstreamVersion = upstreamVersion;
     this.renderError();
-    node.textContent = `${upstreamVersion} available`;
+    node.classList.add(bulmaHoverableClass);
+    node.addEventListener('click', this.handleClick.bind(this));
+    node.textContent = `${this.upstreamVersion} available`;
   }
 
   public showNoConnection() {
     this.renderWarning();
     node.textContent = 'No internet';
+  }
+
+  private handleClick() {
+    window.open(
+      `https://github.com/sound-scape-explorer/sound-scape-explorer/releases/tag/v${this.upstreamVersion}`,
+    );
   }
 }
