aac745ca569235cde1b9954b8cd68b75a23ea30d
Bamdad Sabbagh <hi@bamdad.fr>
2026-01-12 13:14:55 +0100

    fix(front): prevent calendar start > end dates

--- front/src/components/timeline/use-timeline-range.ts
+++ front/src/components/timeline/use-timeline-range.ts
@@ -1,13 +1,18 @@
+import {useAppNotification} from 'src/app/notification/use-app-notification';
 import {ref} from 'vue';
+
+const DEFAULT = 0;
 
 const start = ref<number>(0); // ms
 const end = ref<number>(0); // ms
 
-const left = ref<number>(0);
-const right = ref<number>(0);
+const left = ref<number>(DEFAULT);
+const right = ref<number>(DEFAULT);
 const duration = ref<number>(0);
 
 export function useTimelineRange() {
+  const {notify} = useAppNotification();
+
   const moveCursor = (deltaPercent: number) => {
     let newLeft = left.value + deltaPercent;
     let newRight = newLeft + duration.value;
@@ -45,11 +50,21 @@ export function useTimelineRange() {
   };
 
   const updateLeft = (newLeft: number) => {
+    if (newLeft >= right.value && right.value !== DEFAULT) {
+      notify('error', 'Calendar', 'Start date is after end date');
+      return;
+    }
+
     left.value = newLeft;
     updateDuration();
   };
 
   const updateRight = (newRight: number) => {
+    if (newRight <= left.value && left.value !== DEFAULT) {
+      notify('error', 'Calendar', 'End date is before start date');
+      return;
+    }
+
     right.value = newRight;
     updateDuration();
   };
